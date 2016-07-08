99ef40ed94916c4262f506af32f7e81438baddb6
Panos <PanosConroe@hotmail.com>
2016-07-09 00:17:20 +0300

    Implemented NewScene() and CreateEmptyGameObject() events.

--- Editor/DirectusTreeWidget.h
+++ Editor/DirectusTreeWidget.h
@@ -10,12 +10,19 @@ class DirectusTreeWidget : public QTreeWidget
     Q_OBJECT
 public:
     explicit DirectusTreeWidget(QWidget* parent = 0);
+    void SetEngineSocket(Socket* socket);
 
 private:
     Socket* m_socket;
-    void Update();
+    void Clear();
 
 signals:
 
 public slots:
+     void Populate();
+     void CreateEmptyGameObject();
+     void NewScene();
+     void OpenScene();
+     void SaveScene();
+     void SaveSceneAs();
 };
