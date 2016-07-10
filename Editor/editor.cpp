614bc34ab929e21868daf820ecbdf855716aef5e
Panos <PanosConroe@hotmail.com>
2016-07-10 07:07:40 +0300

    Implemented a directory and a file explorer under the tab "Project", they are both not fully functional yet. Also, added about dialog and did various UI improvements.

--- Editor/editor.cpp
+++ Editor/editor.cpp
@@ -1,18 +1,32 @@
+//= INCLUDES =========
 #include "editor.h"
 #include "ui_editor.h"
+//====================
 
-Editor::Editor(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::Editor)
+Editor::Editor(QWidget* parent) : QMainWindow(parent), ui(new Ui::Editor)
 {
     ui->setupUi(this);
 
+    // Create whatever we need here
+    m_aboutDialog = new AboutDialog(this);
+
+    // Get engine socket
     Socket* engineSocket = ui->directus3DWidget->GetEngineSocket();
+
+    // Pass the engine socket to the widgets that need it
     ui->hierarchyTree->SetEngineSocket(engineSocket);
     ui->consoleList->SetEngineSocket(engineSocket);
+
+    // Resolve other dependencies
+    ui->projectTreeView->SetFileExplorer(ui->projectListView);
 }
 
 Editor::~Editor()
 {
     delete ui;
+}
+
+void Editor::on_actionAbout_Directus3D_triggered()
+{
+    m_aboutDialog->show();
 }
