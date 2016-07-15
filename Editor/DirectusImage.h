cfde1477ce634398faa30e30fe12bcb5f4c31407
Panos <PanosConroe@hotmail.com>
2016-07-15 10:20:35 +0300

    Created DirectusImage, it allows for asynchronous image loading of pretty much any type, this is achieved by using Directus3D instead of Qt.

---
+++ Editor/DirectusImage.h
@@ -0,0 +1,17 @@
+#ifndef DIRECTUSIMAGE_H
+#define DIRECTUSIMAGE_H
+
+#include <QWidget>
+
+class DirectusImage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DirectusImage(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // DIRECTUSIMAGE_H
