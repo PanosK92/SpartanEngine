4d2a06a0ed7d6f3892e25e203e03ec301faddb93
Panos <PanosConroe@hotmail.com>
2016-07-31 03:31:05 +0300

    Added an option button on the top right of each component. Upon clicking on it, a context menu pops up, for the time being you can only click on "Remove Component" to remove the component at hand. The context menu will receive an aesthetic makeover.

---
+++ Editor/DirectusDropDownButton.h
@@ -0,0 +1,17 @@
+#ifndef DIRECTUSDROPDOWNBUTTON_H
+#define DIRECTUSDROPDOWNBUTTON_H
+
+#include <QWidget>
+
+class DirectusDropDownButton : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DirectusDropDownButton(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // DIRECTUSDROPDOWNBUTTON_H
