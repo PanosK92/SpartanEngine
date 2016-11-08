58fc04df56ebbab1ec7fca92b412a1f35604cbe8
Panos Karabelas <panosconroe@hotmail.com>
2016-11-08 17:26:31 +0200

    Moved the definitions of component related templated functions of GameObject to the header, and removed all the explicit declarations.

--- Directus3D/Core/GUIDGenerator.cpp
+++ Directus3D/Core/GUIDGenerator.cpp
@@ -21,14 +21,41 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #pragma once
 
-//= INCLUDES ====
-#include <string>
-//===============
+//= INCLUDES =============
+#include "GUIDGenerator.h"
+#include <iomanip>
+#include <sstream> 
+#include <guiddef.h>
+#include "Objbase.h"
+#include <winerror.h>
+//========================
 
-#define GENERATE_GUID GUIDGenerator::Generate()
+//= NAMESPACES =====
+using namespace std;
+//==================
 
-class __declspec(dllexport) GUIDGenerator
+string GUIDGenerator::Generate()
 {
-public:
-	static std::string Generate();
-};
+	string guidString = "N/A";
+	GUID guid;	
+	HRESULT hr = CoCreateGuid(&guid);
+	if (SUCCEEDED(hr))
+	{
+		stringstream stream;
+		stream << hex << uppercase
+			<< setw(8) << setfill('0') << guid.Data1
+			<< "-" << setw(4) << setfill('0') << guid.Data2
+			<< "-" << setw(4) << setfill('0') << guid.Data3
+			<< "-";
+
+		for (int i = 0; i < sizeof(guid.Data4); ++i)
+		{
+			if (i == 2)
+				stream << "-";
+			stream << hex << setw(2) << setfill('0') << int(guid.Data4[i]);
+		}
+		guidString = stream.str();
+	}
+
+	return guidString;
+}
--- Directus3D/Core/GameObject.h
+++ Directus3D/Core/GameObject.h
@@ -26,6 +26,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "../Components/IComponent.h"
 #include <vector>
 #include "../Core/Context.h"
+#include "GUIDGenerator.h"
+#include "Scene.h"
 //===================================
 
 class Transform;
@@ -57,19 +59,110 @@ public:
 
 	//= Components ============================================
 	template <class Type>
-	Type* AddComponent();
+	Type* AddComponent()
+	{
+		// Convert class Type to a string.
+		std::string typeStr(typeid(Type).name());
+		typeStr = typeStr.substr(typeStr.find_first_of(" \t") + 1); // remove word "class".
 
-	template <class Type>
-	Type* GetComponent();
+		// Check if a component of that type already exists
+		IComponent* existingComp = GetComponent<Type>();
+		if (existingComp)
+		{
+			// If it's anything but a script, it can't have multiple
+			// instances return the existing component.
+			if (typeStr != "Script")
+				return dynamic_cast<Type*>(existingComp);
+		}
 
-	template <class Type>
-	std::vector<Type*> GetComponents();
+		// Get the created component.
+		IComponent* component = new Type;
 
-	template <class Type>
-	bool HasComponent();
+		// Add the component.
+		m_components.insert(std::pair<std::string, IComponent*>(typeStr, component));
 
-	template <class Type>
-	void RemoveComponent();
+		// Set default properties.
+		component->g_ID = GENERATE_GUID;
+		component->g_enabled = true;
+
+		// Set some useful pointers.
+		component->g_gameObject = this;
+		component->g_transform = GetTransform();
+		component->g_context = m_context;
+
+		// Run Initialize().
+		component->Initialize();
+
+		// Do a scene analysis
+		m_context->GetSubsystem<Scene>()->Resolve();
+
+		// Return it as a component of the requested type
+		return dynamic_cast<Type*>(component);
+	}
+
+	// Returns a component of type T (if it exists)
+	template <class T>
+	T* GetComponent()
+	{
+		for (auto const& it : m_components)
+		{
+			// casting failure == nullptr
+			T* typed_cmp = dynamic_cast<T*>(it.second);
+			if (dynamic_cast<T*>(it.second))
+				return typed_cmp;
+		}
+
+		return nullptr;
+	}
+
+	// Returns any components of type T (if they exist)
+	template <class T>
+	std::vector<T*> GetComponents()
+	{
+		std::vector<T*> components;
+		for (auto const& it : m_components)
+		{
+			// casting failure == nullptr
+			T* typed_cmp = dynamic_cast<T*>(it.second);
+			if (typed_cmp)
+				components.push_back(typed_cmp);
+		}
+
+		return components;
+	}
+
+	// Checks if a component of type T exists
+	template <class T>
+	bool HasComponent()
+	{
+		if (GetComponent<T>())
+			return true;
+
+		return false;
+	}
+
+	// Removes a component of type T (if it exists)
+	template <class T>
+	void RemoveComponent()
+	{
+		for (auto it = m_components.begin(); it != m_components.end();)
+		{
+			IComponent* component = it->second;
+
+			// casting failure == nullptr
+			T* typed_cmp = dynamic_cast<T*>(component);
+			if (typed_cmp)
+			{
+				component->Remove();
+				delete component;
+				it = m_components.erase(it);
+				continue;
+			}
+			++it;
+		}
+
+		m_context->GetSubsystem<Scene>()->Resolve();
+	}
 
 	void RemoveComponentByID(const std::string& id);
 	//=========================================================
