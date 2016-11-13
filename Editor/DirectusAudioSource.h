628aa7c5c130ab777b26abda3c74686c664c95d1
Panos Karabelas <panosconroe@hotmail.com>
2016-11-13 12:49:53 +0200

    Overall audio support improvements.

---
+++ Editor/DirectusAudioSource.h
@@ -0,0 +1,17 @@
+#ifndef DIRECTUSAUDIOSOURCE_H
+#define DIRECTUSAUDIOSOURCE_H
+
+#include <QWidget>
+
+class DirectusAudioSource : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DirectusAudioSource(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // DIRECTUSAUDIOSOURCE_H
