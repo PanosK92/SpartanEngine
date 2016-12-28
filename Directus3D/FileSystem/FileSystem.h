16a37d5fe0b07cdd4c0a703b81eeae3d232ed74b
Panos Karabelas <panosconroe@hotmail.com>
2016-12-28 11:26:49 +0200

    Renamed "Compiled x64" to "mvcs140 x64" and included all the libs via "#pragma comment".

--- Directus3D/FileSystem/FileSystem.h
+++ Directus3D/FileSystem/FileSystem.h
@@ -21,6 +21,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #pragma once
 
+//= LINKING ===============================
+#pragma comment(lib, "assimp-vc140-mt.lib")
+//=========================================
+
 //= INCLUDES ==============
 #include <vector>
 #include "../Core/Helper.h"
