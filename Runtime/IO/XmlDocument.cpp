e0ef5583cbbe32a3330df8ecb14877420e5220e5
Panos Karabelas <panosconroe@hotmail.com>
2017-08-05 20:47:38 +0100

    Renamed XMLIO to XmlDocument

--- Runtime/IO/XmlDocument.cpp
+++ Runtime/IO/XmlDocument.cpp
@@ -20,7 +20,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 //= INCLUDES ==============
-#include "XmlIO.h"
+#include "XmlDocument.h"
 #include "pugixml.hpp"
 #include "../Logging/Log.h"
 //=========================
