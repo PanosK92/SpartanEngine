6141ff9b3e31f2f64379666ba8d7a0f454454a77
Panos Karabelas <panosconroe@hotmail.com>
2017-09-16 00:35:18 +0100

    Improved accuracy of frame and render times in the editor

--- Editor/DirectusStatsLabel.cpp
+++ Editor/DirectusStatsLabel.cpp
@@ -21,13 +21,13 @@ void DirectusStatsLabel::UpdateStats(DirectusViewport* directusViewport)
     Context* context = directusViewport->GetEngineContext();
 
     float fps = context->GetSubsystem<Scene>()->GetFPS();
-    double render = context->GetSubsystem<Renderer>()->GetRenderTime();
-    double frame = context->GetSubsystem<Timer>()->GetDeltaTimeMs();
+    float render = context->GetSubsystem<Renderer>()->GetRenderTime();
+    float frame = context->GetSubsystem<Timer>()->GetDeltaTimeMs();
     int meshes = context->GetSubsystem<Renderer>()->GetRenderedMeshesCount();
 
     string fpsStr = "FPS: " + FormatFloat(fps, 2);
-    string frameStr = "Frame: " + FormatDouble(frame, 2) + (" ms");
-    string renderStr = "Render: " + FormatDouble(render, 2) + (" ms");
+    string frameStr = "Frame: " + FormatFloat(frame, 2) + (" ms");
+    string renderStr = "Render: " + FormatFloat(render, 2) + (" ms");
     string meshesStr = "Meshes Rendered: " + to_string(meshes);
 
     string finalText = fpsStr + ", " + frameStr + ", " + renderStr + ", " + meshesStr;
@@ -35,15 +35,6 @@ void DirectusStatsLabel::UpdateStats(DirectusViewport* directusViewport)
 }
 
 string DirectusStatsLabel::FormatFloat(float value, int digitsAfterDecimal)
-{
-    QString str = QString::number(value, 'f', digitsAfterDecimal);
-    str.remove( QRegExp("0+$") ); // Remove any number of trailing 0's
-    str.remove( QRegExp("\\.$") ); // If the last character is just a '.' then remove it
-
-    return str.toStdString();
-}
-
-string DirectusStatsLabel::FormatDouble(double value, int digitsAfterDecimal)
 {
     QString str = QString::number(value, 'f', digitsAfterDecimal);
     str.remove( QRegExp("0+$") ); // Remove any number of trailing 0's
