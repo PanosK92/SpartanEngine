311df46c1c72c79b74425059a2b202f58563e081
Panos Karabelas <panosconroe@hotmail.com>
2017-10-21 01:30:18 +0100

    Added lock guard

--- Runtime/Logging/Log.h
+++ Runtime/Logging/Log.h
@@ -101,9 +101,10 @@ namespace Directus
 		template<typename T>
 		static void Write(std::shared_ptr<T> ptr, LogType type) { Write(ptr ? typeid(ptr).name() : "Null", type); }
 
-		static void WriteToFile(const std::string& text);
-
 	private:
+		static void LogString(const std::string& text, LogType type);
+		static void WriteToFile(const std::string& text, LogType type);
+
 		static std::weak_ptr<ILogger> m_logger;
 		static std::ofstream m_fout;
 		static std::string m_logFileName;
