50d3e1416a6f6ee4177dd8de0dfcaddc5854e4ba
PanosK92 <panosconroe@hotmail.com>
2018-02-10 16:48:27 +0000

    Moved all graphics API specific definitions into EngineBackend.h

--- Runtime/Input/DX8Input.h
+++ Runtime/Input/DX8Input.h
@@ -21,13 +21,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #pragma once
 
-#define DIRECTINPUT_VERSION 0x0800
-
-//= LINKING ===========================
-#pragma comment(lib, "dinput8.lib")
-#pragma comment(lib, "dxguid.lib")
-//=====================================
-
 //= INCLUDES ===============
 #include "../Math/Vector3.h"
 #include <dinput.h>
