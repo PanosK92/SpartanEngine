7caf4849d5b11e2b0bcf16c5021e553786e3c322
PanosK92 <panosconroe@hotmail.com>
2018-02-11 02:57:41 +0000

    Added missing play button icon

--- Generate_VS17_Project.bat
+++ Generate_VS17_Project.bat
@@ -1,5 +1,7 @@
 @echo off
+:: Copy required assets to build directory
 xcopy "Runtime\Assets" "Binaries\Release\Standard Assets\" /E /I
+:: Generation VS solution
 cd Build_Scripts
 premake5 vs2017
 pause
