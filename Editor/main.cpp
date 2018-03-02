4f4ed7b397140a4516743761907bce656f131ef6
PanosK92 <panosconroe@hotmail.com>
2018-03-02 22:09:35 +0000

    Updated SDL to latest version. SDL is now statically linked

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -19,6 +19,10 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+//= DEFINES ============
+#define SDL_MAIN_HANDLED
+//======================
+
 //= INCLUDES =================
 #include <iostream>
 #include <SDL.h>
