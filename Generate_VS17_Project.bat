9935bffd7cf1468a7d51c805378f7bfc74252cbb
Panos Karabelas <panosconroe@hotmail.com>
2018-03-06 16:22:04 +0000

    Moved DLL dependencies into ThirdParty

--- Generate_VS17_Project.bat
+++ Generate_VS17_Project.bat
@@ -1,9 +1,9 @@
 @echo off
 :: Copy required assets to build directory
-xcopy "Prerequisites\Standard Assets" "Binaries\Debug\Standard Assets\" /E /I
-xcopy "Prerequisites\Standard Assets" "Binaries\Release\Standard Assets\" /E /I
-xcopy "Prerequisites\DLLs\fmodL64.dll" "Binaries\Debug\" /E /I
-xcopy "Prerequisites\DLLs\fmod64.dll" "Binaries\Release\" /E /I
+xcopy "Assets\Standard Assets" "Binaries\Debug\Standard Assets\" /E /I
+xcopy "Assets\Standard Assets" "Binaries\Release\Standard Assets\" /E /I
+xcopy "ThirdParty\mvsc141_x64\fmodL64.dll" "Binaries\Debug\" /E /I
+xcopy "ThirdParty\mvsc141_x64\fmod64.dll" "Binaries\Release\" /E /I
 :: Generation VS solution
 cd Build_Scripts
 premake5 vs2017
