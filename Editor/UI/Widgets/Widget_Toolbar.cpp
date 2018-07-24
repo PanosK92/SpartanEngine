e40b29dd419178b51e2b00b99d59ff64f9d3fb80
Panos Karabelas <panosconroe@hotmail.com>
2018-07-24 18:03:40 +0100

    Replaced array with vector and wrapped all variables inside a namespace

--- Editor/UI/Widgets/Widget_Toolbar.cpp
+++ Editor/UI/Widgets/Widget_Toolbar.cpp
@@ -33,29 +33,30 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 using namespace Directus;
 //=======================
 
-static float g_buttonSize = 20.0f;
-
-static bool g_showRendererOptions	= false;
-static bool g_physics = true;
-static bool g_aabb = false;
-static bool g_gizmos = true;
-static bool g_pickingRay = false;
-static bool g_grid = true;
-static bool g_performanceMetrics = false;
-const char* g_rendererViews[] =
+namespace Toolbar_Options
 {
-	"Default",
-	"Albedo",
-	"Normal",
-	"Specular",
-	"Depth"
-};
-static int g_rendererViewInt = 0;
-static const char* g_rendererView = g_rendererViews[g_rendererViewInt];
+	static float g_buttonSize = 20.0f;
+	static bool g_showRendererOptions	= false;
+	static bool g_physics = true;
+	static bool g_aabb = false;
+	static bool g_gizmos = true;
+	static bool g_pickingRay = false;
+	static bool g_grid = true;
+	static bool g_performanceMetrics = false;
+	std::vector<char*> g_rendererViews = {
+		"Default",
+		"Albedo",
+		"Normal",
+		"Specular",
+		"Depth"
+	};
+	static int g_rendererViewInt = 0;
+	static const char* g_rendererView = g_rendererViews[g_rendererViewInt];
+}
 
 Widget_Toolbar::Widget_Toolbar()
 {
-	
+
 }
 
 void Widget_Toolbar::Initialize(Context* context)
@@ -88,7 +89,7 @@ void Widget_Toolbar::Update(float deltaTime)
 	// Play button
 	ImGui::SameLine();
 	ImGui::PushStyleColor(ImGuiCol_Button, Engine::EngineMode_IsSet(Engine_Game) ? ImGui::GetStyle().Colors[ImGuiCol_ButtonActive] : ImGui::GetStyle().Colors[ImGuiCol_Button]);
-	if (THUMBNAIL_BUTTON_BY_TYPE(Icon_Button_Play, g_buttonSize))
+	if (THUMBNAIL_BUTTON_BY_TYPE(Icon_Button_Play, Toolbar_Options::g_buttonSize))
 	{
 		Engine::EngineMode_Toggle(Engine_Game);
 	}
@@ -96,34 +97,34 @@ void Widget_Toolbar::Update(float deltaTime)
 
 	// Renderer options button
 	ImGui::SameLine();
-	ImGui::PushStyleColor(ImGuiCol_Button, g_showRendererOptions ? ImGui::GetStyle().Colors[ImGuiCol_ButtonActive] : ImGui::GetStyle().Colors[ImGuiCol_Button]);
-	if (THUMBNAIL_BUTTON_BY_TYPE(Icon_Component_Options, g_buttonSize))
+	ImGui::PushStyleColor(ImGuiCol_Button, Toolbar_Options::g_showRendererOptions ? ImGui::GetStyle().Colors[ImGuiCol_ButtonActive] : ImGui::GetStyle().Colors[ImGuiCol_Button]);
+	if (THUMBNAIL_BUTTON_BY_TYPE(Icon_Component_Options, Toolbar_Options::g_buttonSize))
 	{
-		g_showRendererOptions = true;
+		Toolbar_Options::g_showRendererOptions = true;
 	}
 	ImGui::PopStyleColor();
 
 	ImGui::PopStyleVar();
 
 	// Visibility
-	if (g_showRendererOptions)	ShowRendererOptions();	
+	if (Toolbar_Options::g_showRendererOptions)	ShowRendererOptions();	
 }
 
 void Widget_Toolbar::ShowRendererOptions()
 {
-	ImGui::Begin("Renderer Options", &g_showRendererOptions, ImGuiWindowFlags_AlwaysAutoResize);
+	ImGui::Begin("Renderer Options", &Toolbar_Options::g_showRendererOptions, ImGuiWindowFlags_AlwaysAutoResize);
 
 	// G-Buffer Visualization
 	{
-		if (ImGui::BeginCombo("G-Buffer", g_rendererView))
+		if (ImGui::BeginCombo("G-Buffer", Toolbar_Options::g_rendererView))
 		{
-			for (int i = 0; i < IM_ARRAYSIZE(g_rendererViews); i++)
+			for (auto i = 0; i < Toolbar_Options::g_rendererViews.size(); i++)
 			{
-				bool is_selected = (g_rendererView == g_rendererViews[i]);
-				if (ImGui::Selectable(g_rendererViews[i], is_selected))
+				bool is_selected = (Toolbar_Options::g_rendererView == Toolbar_Options::g_rendererViews[i]);
+				if (ImGui::Selectable(Toolbar_Options::g_rendererViews[i], is_selected))
 				{
-					g_rendererView		= g_rendererViews[i];
-					g_rendererViewInt	= i;
+					Toolbar_Options::g_rendererView		= Toolbar_Options::g_rendererViews[i];
+					Toolbar_Options::g_rendererViewInt	= i;
 				}
 				if (is_selected)
 				{
@@ -133,35 +134,35 @@ void Widget_Toolbar::ShowRendererOptions()
 			ImGui::EndCombo();
 		}
 
-		if (g_rendererViewInt == 0) // Combined
+		if (Toolbar_Options::g_rendererViewInt == 0) // Combined
 		{
 			Renderer::RenderFlags_Disable(Render_Albedo);
 			Renderer::RenderFlags_Disable(Render_Normal);
 			Renderer::RenderFlags_Disable(Render_Specular);
 			Renderer::RenderFlags_Disable(Render_Depth);
 		}
-		else if (g_rendererViewInt == 1) // Albedo
+		else if (Toolbar_Options::g_rendererViewInt == 1) // Albedo
 		{
 			Renderer::RenderFlags_Enable(Render_Albedo);
 			Renderer::RenderFlags_Disable(Render_Normal);
 			Renderer::RenderFlags_Disable(Render_Specular);
 			Renderer::RenderFlags_Disable(Render_Depth);
 		}
-		else if (g_rendererViewInt == 2) // Normal
+		else if (Toolbar_Options::g_rendererViewInt == 2) // Normal
 		{
 			Renderer::RenderFlags_Disable(Render_Albedo);
 			Renderer::RenderFlags_Enable(Render_Normal);
 			Renderer::RenderFlags_Disable(Render_Specular);
 			Renderer::RenderFlags_Disable(Render_Depth);
 		}
-		else if (g_rendererViewInt == 3) // Specular
+		else if (Toolbar_Options::g_rendererViewInt == 3) // Specular
 		{
 			Renderer::RenderFlags_Disable(Render_Albedo);
 			Renderer::RenderFlags_Disable(Render_Normal);
 			Renderer::RenderFlags_Enable(Render_Specular);
 			Renderer::RenderFlags_Disable(Render_Depth);
 		}
-		else if (g_rendererViewInt == 4) // Depth
+		else if (Toolbar_Options::g_rendererViewInt == 4) // Depth
 		{
 			Renderer::RenderFlags_Disable(Render_Albedo);
 			Renderer::RenderFlags_Disable(Render_Normal);
@@ -197,19 +198,19 @@ void Widget_Toolbar::ShowRendererOptions()
 
 	// Misc
 	{
-		ImGui::Checkbox("Physics", &g_physics);
-		ImGui::Checkbox("AABB", &g_aabb);
-		ImGui::Checkbox("Gizmos", &g_gizmos);
-		ImGui::Checkbox("Picking Ray", &g_pickingRay);
-		ImGui::Checkbox("Scene Grid", &g_grid);
-		ImGui::Checkbox("Performance Metrics", &g_performanceMetrics);
+		ImGui::Checkbox("Physics", &Toolbar_Options::g_physics);
+		ImGui::Checkbox("AABB", &Toolbar_Options::g_aabb);
+		ImGui::Checkbox("Gizmos", &Toolbar_Options::g_gizmos);
+		ImGui::Checkbox("Picking Ray", &Toolbar_Options::g_pickingRay);
+		ImGui::Checkbox("Scene Grid", &Toolbar_Options::g_grid);
+		ImGui::Checkbox("Performance Metrics", &Toolbar_Options::g_performanceMetrics);
 
-		g_physics				? Renderer::RenderFlags_Enable(Render_Physics)				: Renderer::RenderFlags_Disable(Render_Physics);
-		g_aabb					? Renderer::RenderFlags_Enable(Render_AABB)					: Renderer::RenderFlags_Disable(Render_AABB);
-		g_gizmos				? Renderer::RenderFlags_Enable(Render_Light)				: Renderer::RenderFlags_Disable(Render_Light);
-		g_pickingRay			? Renderer::RenderFlags_Enable(Render_PickingRay)			: Renderer::RenderFlags_Disable(Render_PickingRay);
-		g_grid					? Renderer::RenderFlags_Enable(Render_SceneGrid)			: Renderer::RenderFlags_Disable(Render_SceneGrid);
-		g_performanceMetrics	? Renderer::RenderFlags_Enable(Render_PerformanceMetrics)	: Renderer::RenderFlags_Disable(Render_PerformanceMetrics);
+		Toolbar_Options::g_physics				? Renderer::RenderFlags_Enable(Render_Physics)				: Renderer::RenderFlags_Disable(Render_Physics);
+		Toolbar_Options::g_aabb					? Renderer::RenderFlags_Enable(Render_AABB)					: Renderer::RenderFlags_Disable(Render_AABB);
+		Toolbar_Options::g_gizmos				? Renderer::RenderFlags_Enable(Render_Light)				: Renderer::RenderFlags_Disable(Render_Light);
+		Toolbar_Options::g_pickingRay			? Renderer::RenderFlags_Enable(Render_PickingRay)			: Renderer::RenderFlags_Disable(Render_PickingRay);
+		Toolbar_Options::g_grid					? Renderer::RenderFlags_Enable(Render_SceneGrid)			: Renderer::RenderFlags_Disable(Render_SceneGrid);
+		Toolbar_Options::g_performanceMetrics	? Renderer::RenderFlags_Enable(Render_PerformanceMetrics)	: Renderer::RenderFlags_Disable(Render_PerformanceMetrics);
 	}
 
 	ImGui::End();
