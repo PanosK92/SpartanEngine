10abb039c16902f02ce3a7145112ac752643641c
Panos Karabelas <PanosConroe@hotmail.com>
2018-12-09 20:44:14 +0000

    Improved TAA

--- Assets/Standard Assets/Shaders/ResolveTAA.hlsl
+++ Assets/Standard Assets/Shaders/ResolveTAA.hlsl
@@ -1,35 +1,39 @@
-// Based on "Temporal Antialiasing In Uncharted 4" by Ke XU
+// Based on "Temporal Antialiasing In Uncharted 4" by Ke XU - Naughty Dog.
+// Clamping based on Playdead's INSIDE approach.
 
-static const float g_blendMin = 0.05f;
-static const float g_blendMax = 0.8f;
+static const float g_blendMin = 0.0f;
+static const float g_blendMax = 1.0f;
 
 float4 ResolveTAA(float2 texCoord, Texture2D tex_history, Texture2D tex_current, Texture2D tex_velocity, Texture2D tex_depth, SamplerState sampler_bilinear)
 {
-	// Reproject texture coordinates
-	float2 velocity			= GetVelocity_Dilate_Depth(texCoord, tex_current, tex_depth, sampler_bilinear) * g_texelSize;
-    float2 texCoord_history	= texCoord - velocity;
+	// Get velocity for nearest depth (and the depth)
+	float depth 	= 1.0f;
+	float2 velocity	= GetVelocity_Dilate_Depth(texCoord, tex_current, tex_depth, sampler_bilinear, depth) * g_texelSize;	
 	
-	//= Clamp out too different history colors - For non-existing and lighting change cases ==========
-	float2 du = float2(g_texelSize.x, 0.0f);
-	float2 dv = float2(0.0f, g_texelSize.y);
+	float4 color_current 	= tex_current.Sample(sampler_bilinear, texCoord);
+	float4 color_history 	= tex_history.Sample(sampler_bilinear, texCoord - velocity);
 
-	float4 ctl = tex_current.Sample(sampler_bilinear, texCoord - dv - du);
-	float4 ctc = tex_current.Sample(sampler_bilinear, texCoord - dv);
-	float4 ctr = tex_current.Sample(sampler_bilinear, texCoord - dv + du);
-	float4 cml = tex_current.Sample(sampler_bilinear, texCoord - du);
-	float4 cmc = tex_current.Sample(sampler_bilinear, texCoord);
-	float4 cmr = tex_current.Sample(sampler_bilinear, texCoord + du);
-	float4 cbl = tex_current.Sample(sampler_bilinear, texCoord + dv - du);
-	float4 cbc = tex_current.Sample(sampler_bilinear, texCoord + dv);
-	float4 cbr = tex_current.Sample(sampler_bilinear, texCoord + dv + du);
-	
-	float4 cMin = min(ctl, min(ctc, min(ctr, min(cml, min(cmc, min(cmr, min(cbl, min(cbc, cbr))))))));
-	float4 cMax = max(ctl, max(ctc, max(ctr, max(cml, max(cmc, max(cmr, max(cbl, max(cbc, cbr))))))));
+	//= Clamp out too different history colors - For non-existing and lighting change cases ==========
+	const float _SubpixelThreshold 		= 0.5f;
+	const float _GatherBase 			= 0.5f;
+	const float _GatherSubpixelMotion 	= 0.5f;
+
+	float2 texel_vel 		= velocity / g_texelSize;
+	float texel_vel_mag 	= length(texel_vel) * depth;
+	float k_subpixel_motion = saturate(_SubpixelThreshold / (EPSILON + texel_vel_mag));
+	float k_min_max_support = _GatherBase + _GatherSubpixelMotion * k_subpixel_motion;
+
+	float2 ss_offset01 	= k_min_max_support * float2(-g_texelSize.x, g_texelSize.y);
+	float2 ss_offset11 	= k_min_max_support * float2(g_texelSize.x, g_texelSize.y);
+	float4 c00 			= tex_current.Sample(sampler_bilinear, texCoord - ss_offset11);
+	float4 c10 			= tex_current.Sample(sampler_bilinear, texCoord - ss_offset01);
+	float4 c01 			= tex_current.Sample(sampler_bilinear, texCoord + ss_offset01);
+	float4 c11 			= tex_current.Sample(sampler_bilinear, texCoord + ss_offset11);
+
+	float4 cMin 	= min(c00, min(c10, min(c01, c11)));
+	float4 cMax 	= max(c00, max(c10, max(c01, c11)));
+	color_history 	= clamp(color_history, cMin, cMax);
 	//================================================================================================
-	
-	// Get current and history color
-	float4 color_current 	= cmc;
-	float4 color_history 	= clamp(tex_history.Sample(sampler_bilinear, texCoord_history), cMin, cMax);
 	
 	//= Compute feedback weight from unbiased luminance difference (Timothy Lottes)=
 	float lum0 					= Luminance(color_current);
