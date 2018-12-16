f566b261f0d7cb90cc643595b5a02653ed717212
Panos Karabelas <PanosConroe@hotmail.com>
2018-12-16 23:20:18 +0000

    Improved TAA (better anti-aliasing, more stable)

--- Assets/Standard Assets/Shaders/MotionBlur.hlsl
+++ Assets/Standard Assets/Shaders/MotionBlur.hlsl
@@ -1,9 +1,9 @@
 static const int MAX_SAMPLES = 16;
 
-float4 MotionBlur(float2 texCoord, Texture2D texture_color, Texture2D texture_velocity, SamplerState bilinearSampler)
+float4 MotionBlur(float2 texCoord, Texture2D texture_color, Texture2D texture_velocity, SamplerState sampler_bilinear)
 {	
-	float4 color 	= texture_color.Sample(bilinearSampler, texCoord);	
-	float2 velocity = GetVelocity_Dilate_Average(texCoord, texture_velocity, bilinearSampler);
+	float4 color 	= texture_color.Sample(sampler_bilinear, texCoord);	
+	float2 velocity = GetVelocity_Dilate_Average(texCoord, texture_velocity, sampler_bilinear);
 	
 	// Make velocity scale based on user preference instead of frame rate
 	float velocity_scale 	= (g_fps_current / g_fps_target) * g_motionBlur_strength;
@@ -20,7 +20,7 @@ float4 MotionBlur(float2 texCoord, Texture2D texture_color, Texture2D texture_ve
 	for (int i = 1; i < samples; ++i) 
 	{
 		float2 offset 	= velocity * (float(i) / float(samples - 1) - 0.5f);
-		color 			+= texture_color.SampleLevel(bilinearSampler, texCoord + offset, 0);
+		color 			+= texture_color.SampleLevel(sampler_bilinear, texCoord + offset, 0);
 	}
 	color /= float(samples);
 
