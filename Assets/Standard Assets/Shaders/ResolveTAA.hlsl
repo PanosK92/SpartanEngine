6416014c0a80b88768c0a056e4226bdbd3ac6939
Panos Karabelas <PanosConroe@hotmail.com>
2018-12-18 08:13:28 +0000

    Fixed a bug where subpixel velocity computation was not using texel velocity.

--- Assets/Standard Assets/Shaders/ResolveTAA.hlsl
+++ Assets/Standard Assets/Shaders/ResolveTAA.hlsl
@@ -7,10 +7,10 @@ float4 ResolveTAA(float2 texCoord, Texture2D tex_history, Texture2D tex_current,
 {
 	// Get velocity for nearest depth (and the depth)
 	float depth 	= 1.0f;
-	float2 velocity	= GetVelocity_Dilate_Depth3X3(texCoord, tex_velocity, tex_depth, sampler_bilinear, depth);
+	float2 velocity	= GetVelocity_Dilate_Depth3X3(texCoord, tex_velocity, tex_depth, sampler_bilinear, depth) * g_texelSize;
 	
 	float4 color_current 	= tex_current.Sample(sampler_bilinear, texCoord);
-	float2 texCoord_history = texCoord - velocity * g_texelSize;
+	float2 texCoord_history = texCoord - velocity;
 	float4 color_history 	= tex_history.Sample(sampler_bilinear, texCoord_history);
 
 	//= Clamp out too different history colors - For non-existing and lighting change cases ==========
@@ -33,14 +33,14 @@ float4 ResolveTAA(float2 texCoord, Texture2D tex_history, Texture2D tex_current,
 	color_history = clamp(color_history, cMin, cMax);
 	//================================================================================================
 	
-	//= Compute blend factor based on the amount of subpixel velocity ==========================================================
+	//= Compute blend factor based on the amount of subpixel velocity =================================================
 	float factor_velocity 	= abs(sin(frac(length(velocity)) * PI) - 1.0f); // Decrease when pixel motion gets subpixel
 	float factor_clampMin 	= length(color_history - cMin); 				// Decrease when history is near min clamp
 	float factor_clampMax 	= length(color_history - cMax); 				// Decrease when history is near max clamp
 	//float factor_contrast	= saturate(1.0f - Luminance(cMax - cMin)); 		// Increase when local contrast is low
 	float alpha				= saturate(1.0f - ((factor_velocity + factor_clampMin + factor_clampMax) / 3.0f));
 	float blendfactor 		= lerp(g_blendMin, g_blendMax, alpha);
-	//==========================================================================================================================
+	//=================================================================================================================
 	
 	return lerp(color_history, color_current, blendfactor);
 }
