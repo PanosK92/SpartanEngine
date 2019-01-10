af65dc88fa4dc917ce6052d499c69aefe58d5194
Panos Karabelas <PanosConroe@hotmail.com>
2019-01-10 20:44:10 +0000

    Improved TAA stability (only bloom flicker remains to be dealt with)

--- Assets/Standard Assets/Shaders/ResolveTAA.hlsl
+++ Assets/Standard Assets/Shaders/ResolveTAA.hlsl
@@ -1,7 +1,7 @@
 // Based on "Temporal Antialiasing In Uncharted 4" by Ke XU - Naughty Dog.
 
 static const float g_blendMin = 0.01f;
-static const float g_blendMax = 0.2f;
+static const float g_blendMax = 0.1f;
 
 float4 ResolveTAA(float2 texCoord, Texture2D tex_history, Texture2D tex_current, Texture2D tex_velocity, Texture2D tex_depth, SamplerState sampler_bilinear)
 {
@@ -27,20 +27,20 @@ float4 ResolveTAA(float2 texCoord, Texture2D tex_history, Texture2D tex_current,
 	float4 cbc = tex_current.Sample(sampler_bilinear, texCoord + dv);
 	float4 cbr = tex_current.Sample(sampler_bilinear, texCoord + dv + du);
 	
-	float4 cMin = min(ctl, min(ctc, min(ctr, min(cml, min(cmc, min(cmr, min(cbl, min(cbc, cbr))))))));
-	float4 cMax = max(ctl, max(ctc, max(ctr, max(cml, max(cmc, max(cmr, max(cbl, max(cbc, cbr))))))));
+	float4 color_min = min(ctl, min(ctc, min(ctr, min(cml, min(cmc, min(cmr, min(cbl, min(cbc, cbr))))))));
+	float4 color_max = max(ctl, max(ctc, max(ctr, max(cml, max(cmc, max(cmr, max(cbl, max(cbc, cbr))))))));
 	
-	color_history = clamp(color_history, cMin, cMax);
+	color_history = clamp(color_history, color_min, color_max);
 	//================================================================================================
 	
-	//= Compute blend factor based on the amount of subpixel velocity =================================================
-	float factor_velocity 	= abs(sin(frac(length(velocity)) * PI) - 1.0f); // Decrease when pixel motion gets subpixel
-	float factor_clampMin 	= length(color_history - cMin); 				// Decrease when history is near min clamp
-	float factor_clampMax 	= length(color_history - cMax); 				// Decrease when history is near max clamp
-	//float factor_contrast	= saturate(1.0f - Luminance(cMax - cMin)); 		// Increase when local contrast is low
-	float alpha				= saturate(1.0f - ((factor_velocity + factor_clampMin + factor_clampMax) / 3.0f));
+	//= Compute blend factor based on the amount of subpixel velocity =========================================================
+	float factor_velocity 	= abs(sin(frac(length(velocity)) * PI) - 1.0f); 		// Decrease when pixel motion gets subpixel
+	float factor_clampMin 	= saturate(length(color_history - color_min)); 			// Decrease when history is near min clamp
+	float factor_clampMax 	= saturate(length(color_history - color_max)); 			// Decrease when history is near max clamp
+	float factor_contrast	= 1.0f - saturate(Luminance(color_max - color_min)); 	// Increase when local contrast is low
+	float alpha				= factor_velocity * factor_clampMin * factor_clampMax * factor_contrast;
 	float blendfactor 		= lerp(g_blendMin, g_blendMax, alpha);
-	//=================================================================================================================
+	//=========================================================================================================================
 	
 	return lerp(color_history, color_current, blendfactor);
 }
