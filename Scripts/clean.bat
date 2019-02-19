9a5c32ee61d756144b8e0fbcc8c656ad996ac569
Panos Karabelas <PanosConroe@hotmail.com>
2019-02-19 16:55:37 +0000

    Updated build scripts to delete uncessery files so that the appveyor artifact is much smaller (hopefully)

--- Scripts/clean.bat
+++ Scripts/clean.bat
@@ -1,16 +1,18 @@
 @echo off
 
+:: Set script directory as the working directory
+cd /D "%~dp0"
+
 @RD /S /Q "Binaries\Intermediate"
-del	"Binaries\Release\Runtime.lib"
-del	"Binaries\Release\Runtime.lib"
-del	"Binaries\Release\Editor.lib"
-del	"Binaries\Release\Editor.exp"
-del	"Binaries\Release\Editor.pdb"
-del	"Binaries\Debug\Runtime.lib"
-del	"Binaries\Debug\Runtime.pdb"
-del	"Binaries\Debug\Editor.lib"
-del	"Binaries\Debug\Editor.exp"
-del	"Binaries\Debug\Editor.pdb"
-del	"Binaries\Debug\Editor.ilk"
+del	"..\Binaries\Release\Runtime.lib"
+del	"..\Binaries\Release\Editor.lib"
+del	"..\Binaries\Release\Editor.exp"
+del	"..\Binaries\Release\Editor.pdb"
+del	"..\Binaries\Debug\Runtime.lib"
+del	"..\Binaries\Debug\Runtime.pdb"
+del	"..\Binaries\Debug\Editor.lib"
+del	"..\Binaries\Debug\Editor.exp"
+del	"..\Binaries\Debug\Editor.pdb"
+del	"..\Binaries\Debug\Editor.ilk"
 
 exit /b
