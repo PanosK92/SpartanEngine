f5b2fb79c85407c1bfc6efbac9d5aab0033d0420
Panos Karabelas <PanosConroe@hotmail.com>
2019-02-19 17:27:36 +0000

    Simplified build scripts to never change the working directory (project root by default)

--- Scripts/clean.bat
+++ Scripts/clean.bat
@@ -1,17 +1,14 @@
 @echo off
 
-:: Set script directory as the working directory
-cd /D "%~dp0"
-
 @RD /S /Q "Binaries\Intermediate"
-del	"..\Binaries\Release\Runtime.lib"
-del	"..\Binaries\Release\Editor.lib"
-del	"..\Binaries\Release\Editor.exp"
+del	"Binaries\Release\Runtime.lib"
+del	"Binaries\Release\Editor.lib"
+del	"Binaries\Release\Editor.exp"
 
-del	"..\Binaries\Debug\Editor.exp"
-del	"..\Binaries\Debug\Editor.ilk"
-del	"..\Binaries\Debug\Editor.lib"
-del	"..\Binaries\Debug\Editor.pdb"
-del	"..\Binaries\Debug\Runtime.lib"
+del	"Binaries\Debug\Editor.exp"
+del	"Binaries\Debug\Editor.ilk"
+del	"Binaries\Debug\Editor.lib"
+del	"Binaries\Debug\Editor.pdb"
+del	"Binaries\Debug\Runtime.lib"
 
 exit /b
