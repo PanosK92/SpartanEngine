dafbc3ec8241d8bc2cdbcb3f2f22b58754e7cb01
Panos Karabelas <PanosConroe@hotmail.com>
2019-02-19 16:55:37 +0000

    Updated build scripts to delete uncessery files so that the appveyor artifact is much smaller (hopefully)

--- Generate_VS17_Project.bat
+++ Generate_VS17_Project.bat
@@ -3,14 +3,15 @@
 :: Delete intermediate files from the build directory
 echo 1. Deleting intermediate, lib and pdb files...
 call "Scripts\clean.bat"
+cd /D "%~dp0"
 
 :: Copy required assets to the build directory
 echo 2. Copying required assets to the build directory...
 xcopy "Assets\Standard Assets" "Binaries\Debug\Standard Assets\" /E /I /y
 xcopy "Assets\Standard Assets" "Binaries\Release\Standard Assets\" /E /I /y
 
-:: Copy required DLLs and PDBs to the build directory
-echo 3. Copying required DLLs and PDBs to the build directory...
+:: Copy required DLLs to the build directory
+echo 3. Copying required DLLs to the build directory...
 xcopy "ThirdParty\mvsc141_x64\fmodL64.dll" "Binaries\Debug\" /E /I /Q /y
 xcopy "ThirdParty\mvsc141_x64\fmod64.dll" "Binaries\Release\" /E /I /y
 
