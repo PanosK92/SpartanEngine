627230908bc7646e34ba3c76a0a2a8c73c41dcaa
Panos Karabelas <PanosConroe@hotmail.com>
2019-02-19 17:11:51 +0000

    Disabled symbols in release builds (gets rid of an uncessery pdb file)

--- Generate_VS17_Project.bat
+++ Generate_VS17_Project.bat
@@ -1,21 +1,17 @@
 @echo off
 
-:: Delete intermediate files from the build directory
-echo 1. Deleting intermediate, lib and pdb files...
+echo 1. Deleting intermediate folder and lib files (from the binary directory)...
 call "Scripts\clean.bat"
 cd /D "%~dp0"
 
-:: Copy required assets to the build directory
-echo 2. Copying required assets to the build directory...
+echo 2. Copying required assets to the binary directory...
 xcopy "Assets\Standard Assets" "Binaries\Debug\Standard Assets\" /E /I /y
 xcopy "Assets\Standard Assets" "Binaries\Release\Standard Assets\" /E /I /y
 
-:: Copy required DLLs to the build directory
-echo 3. Copying required DLLs to the build directory...
+echo 3. Copying required DLLs to the binary directory...
 xcopy "ThirdParty\mvsc141_x64\fmodL64.dll" "Binaries\Debug\" /E /I /Q /y
 xcopy "ThirdParty\mvsc141_x64\fmod64.dll" "Binaries\Release\" /E /I /y
 
-:: Generate VS solution
 echo 4. Generating Visual Studio 2017 solution...
 cd Scripts
 premake5 vs2017
