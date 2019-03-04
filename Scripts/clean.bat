5e672cba960c36f56a71e0a93b0ffbe53a7bf56f
Panos Karabelas <PanosConroe@hotmail.com>
2019-03-04 01:59:37 +0000

    Reduced build artifact size

--- Scripts/clean.bat
+++ Scripts/clean.bat
@@ -1,7 +1,7 @@
 @echo off
 
 @RD /S /Q "Binaries\Intermediate"
-del	"Binaries\Release\Runtime.lib"
+del	"Binaries\Release\Engine.lib"
 del	"Binaries\Release\Editor.lib"
 del	"Binaries\Release\Editor.exp"
 
@@ -9,6 +9,6 @@ del	"Binaries\Debug\Editor.exp"
 del	"Binaries\Debug\Editor.ilk"
 del	"Binaries\Debug\Editor.lib"
 del	"Binaries\Debug\Editor.pdb"
-del	"Binaries\Debug\Runtime.lib"
+del	"Binaries\Debug\Engine.lib"
 
 exit /b
