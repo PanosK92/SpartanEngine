a0efb4704e8e78b51723d48b8504a2646c565abf
Panos Karabelas <PanosConroe@hotmail.com>
2019-05-21 21:34:19 +0100

    [Shaders] Fixed some warnings

--- Data/shaders/ParallaxMapping.hlsl
+++ Data/shaders/ParallaxMapping.hlsl
@@ -1,30 +1,40 @@
 // Based on: https://learnopengl.com/Advanced-Lighting/Parallax-Mapping
-float2 ParallaxMapping(Texture2D depth_tex, SamplerState depth_sampler, float2 texCoords, float3 viewDir, float height_scale)
+float2 ParallaxMapping(Texture2D depth_tex, SamplerState depth_sampler, float2 texCoords, float3 view_dir, float3x3 TBN, float height_scale)
 { 
-	height_scale *= -1.0f;
+	float3x3 to_tangent = transpose(TBN);
+	view_dir 			= mul(view_dir, to_tangent);	
+	height_scale 		*= -1.0f;
+
     // number of depth layers
     const float minLayers = 8;
     const float maxLayers = 32;
-    float numLayers = lerp(maxLayers, minLayers, abs(dot(float3(0.0, 0.0, 1.0), viewDir)));  
-    // calculate the size of each layer
+    float numLayers = lerp(maxLayers, minLayers, abs(dot(float3(0.0, 0.0, 1.0), view_dir)));  
+    
+	// calculate the size of each layer
     float layerDepth = 1.0 / numLayers;
+
     // depth of current layer
     float currentLayerDepth = 0.0;
+
     // the amount to shift the texture coordinates per layer (from vector P)
-    float2 P = viewDir.xy / viewDir.z * height_scale; 
+    float2 P = view_dir.xy / view_dir.z * height_scale; 
     float2 deltaTexCoords = P / numLayers;
   
     // get initial values
-    float2  currentTexCoords   = texCoords;
-    float currentDepthMapValue = depth_tex.Sample(depth_sampler, currentTexCoords).r;
+    float2 currentTexCoords = texCoords;
+	float2 deriv_x = ddx_coarse(currentTexCoords);
+	float2 deriv_y = ddy_coarse(currentTexCoords);
+    float currentDepthMapValue = depth_tex.SampleGrad(depth_sampler, currentTexCoords, deriv_x, deriv_y).r;
 
 	[loop]
     while(currentLayerDepth < currentDepthMapValue)
     {
         // shift texture coordinates along direction of P
         currentTexCoords -= deltaTexCoords;
+
         // get depthmap value at current texture coordinates
-        currentDepthMapValue = depth_tex.Sample(depth_sampler, currentTexCoords).r;
+        currentDepthMapValue = depth_tex.SampleGrad(depth_sampler, currentTexCoords, deriv_x, deriv_y).r;
+
         // get depth of next layer
         currentLayerDepth += layerDepth;  
     }
@@ -34,7 +44,7 @@ float2 ParallaxMapping(Texture2D depth_tex, SamplerState depth_sampler, float2 t
 
     // get depth after and before collision for linear interpolation
     float afterDepth  = currentDepthMapValue - currentLayerDepth;
-    float beforeDepth = depth_tex.Sample(depth_sampler, prevTexCoords).r - currentLayerDepth + layerDepth;
+    float beforeDepth = depth_tex.SampleGrad(depth_sampler, prevTexCoords, deriv_x, deriv_y).r - currentLayerDepth + layerDepth;
 	
     // interpolation of texture coordinates
     float weight = afterDepth / (afterDepth - beforeDepth);
