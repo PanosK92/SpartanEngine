ab5ab67b59b899904bd0875ad458e004d981415e
gpuMaster <geoster3d@gmail.com>
2019-08-17 16:39:08 +0300

    Fix  wrong function name

--- Runtime/World/World.cpp
+++ Runtime/World/World.cpp
@@ -119,7 +119,7 @@ namespace Spartan
                      manager->Iterate([&](auto& component)
                      {
                             if (component->IsParentEntityActive())
-                               component->OnStart();
+                               component->OnStop();
                      });
                 });
 			}
