aaba3a6101677058b711e28a812c16fc880e3b95
Panos Karabelas <PanosConroe@hotmail.com>
2019-08-25 14:43:14 +0100

    [ThirdParty] Updated DirectXCompiler to latest

--- Runtime/RHI/Vulkan/Vulkan_Shader.cpp
+++ Runtime/RHI/Vulkan/Vulkan_Shader.cpp
@@ -213,50 +213,6 @@ namespace Spartan
 			return false;
 		}
 
-		inline string utf16_utf8(wstring source)
-		{
-			string destination;
-			wchar_t symbol;
-			unsigned char code0;
-			unsigned char code1;
-			unsigned char code2;
-
-			for (wstring::iterator it = source.begin(); it != source.end(); it++)
-			{
-				symbol = static_cast<wchar_t>(*it);
-
-				code0 = 0;
-				code1 = 0;
-				code2 = 0;
-
-				if (symbol < 0x00000080) 
-				{
-					code0 = static_cast<char>(*it);
-					destination.push_back(code0);
-				}
-				else if (symbol < 0x00000800) 
-				{
-					code0 = symbol % 0x40;
-					code1 = (symbol - code0) / 0x40;
-
-					destination.push_back(0xc0 | code1);
-					destination.push_back(0x80 | code0);
-				}
-				else 
-				{
-					code0 = symbol % 0x40;
-					code1 = ((symbol - code0) / 0x40) % 0x40;
-					code2 = (symbol - code0 - (0x40 * code1)) / 0x1000;
-
-					destination.push_back(0xe0 | code2);
-					destination.push_back(0x80 | code1);
-					destination.push_back(0x80 | code0);
-				}
-			}
-
-			return destination;
-		}
-
 		class Instance
 		{
 		public:
@@ -311,8 +267,9 @@ namespace Spartan
 				{
 					file_name += 2;
 				}
-				
-				string file_name_utf8 = utf16_utf8(file_name);
+
+                // utf16 to utf8, is this done properly ?
+                string file_name_utf8 = CW2A(file_name);
 
 				auto blob_deleter = [](Blob* blob) { delete blob; };
 				unique_ptr<Blob, decltype(blob_deleter)> source(nullptr, blob_deleter);
