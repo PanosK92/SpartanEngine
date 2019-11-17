8a3dc259c4429fc2d27a58de5b373392edbe645e
Panos Karabelas <PanosConroe@hotmail.com>
2019-11-17 23:22:40 +0000

    [Scripts] Simplified scripts and project generation as I've figured out a workaround for AppVeyor instead

--- Scripts/generate_project_files.bat
+++ Scripts/generate_project_files.bat
@@ -1,9 +1,5 @@
 @echo off
 
-:: Make binary output paths
-set bin_debug=Binaries\debug_%~2
-set bin_release=Binaries\release_%~2
-
 echo 1. Deleting intermediate folder and lib files (from the binary directory)...
 call "Scripts\clean.bat"
 echo:
@@ -13,15 +9,15 @@ call Scripts\7z.exe e ThirdParty\libraries\libraries.7z -oThirdParty\libraries\ 
 echo:
 
 echo 3. Copying required data to the binary directory...
-xcopy "Data" "%bin_debug%\data\" 	/E /I /y
-xcopy "Data" "%bin_release%\data\" 	/E /I /y
+xcopy "Data" "Binaries\Debug\Data\" 	/E /I /y
+xcopy "Data" "Binaries\Release\Data\"	/E /I /y
 echo:
 
 echo 4. Copying required DLLs to the binary directory...
-xcopy "ThirdParty\libraries\dxcompiler.dll" "%bin_debug%\" 		/E /I /Q /y
-xcopy "ThirdParty\libraries\fmodL64.dll" 	"%bin_debug%\" 		/E /I /Q /y
-xcopy "ThirdParty\libraries\dxcompiler.dll" "%bin_release%\" 	/E /I /Q /y
-xcopy "ThirdParty\libraries\fmod64.dll" 	"%bin_release%\" 	/E /I /Q /y
+xcopy "ThirdParty\libraries\dxcompiler.dll" "Binaries\Debug\" 		/E /I /Q /y
+xcopy "ThirdParty\libraries\fmodL64.dll" 	"Binaries\Debug\"		/E /I /Q /y
+xcopy "ThirdParty\libraries\dxcompiler.dll" "Binaries\Release\" 	/E /I /Q /y
+xcopy "ThirdParty\libraries\fmod64.dll" 	"Binaries\Release\" 	/E /I /Q /y
 echo:
 
 echo 5. Generating Visual Studio 2019 solution...
