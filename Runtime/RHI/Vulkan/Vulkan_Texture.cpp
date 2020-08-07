6d1522eb6aa7324008a6e1b138edcaa08c12576d
Panos Karabelas <PanosConroe@hotmail.com>
2020-08-07 01:19:48 +0100

    [Vulkan] Some code refactoring

--- Runtime/RHI/Vulkan/Vulkan_Texture.cpp
+++ Runtime/RHI/Vulkan/Vulkan_Texture.cpp
@@ -162,7 +162,7 @@ namespace Spartan
             );
 
             // End/flush
-            if (!vulkan_utility::command_buffer_immediate::end(RHI_Queue_Graphics, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT))
+            if (!vulkan_utility::command_buffer_immediate::end(RHI_Queue_Graphics))
                 return false;
 
             // Free staging buffer
@@ -265,7 +265,7 @@ namespace Spartan
             }
         
             // Flush
-            if (!vulkan_utility::command_buffer_immediate::end(RHI_Queue_Graphics, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT))
+            if (!vulkan_utility::command_buffer_immediate::end(RHI_Queue_Graphics))
             {
                 LOG_ERROR("Failed to end command buffer");
                 return false;
@@ -368,7 +368,7 @@ namespace Spartan
                 return false;
 
             // Flush
-            if (!vulkan_utility::command_buffer_immediate::end(RHI_Queue_Graphics, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT))
+            if (!vulkan_utility::command_buffer_immediate::end(RHI_Queue_Graphics))
                 return false;
 
             // Update this texture with the new layout
