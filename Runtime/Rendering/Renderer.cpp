036f14ea29850bc7c7f6516cc77280ca58385869
Panos Karabelas <PanosConroe@hotmail.com>
2020-08-17 00:20:17 +0100

    [D3D12] Build fix

--- Runtime/Rendering/Renderer.cpp
+++ Runtime/Rendering/Renderer.cpp
@@ -43,6 +43,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "../RHI/RHI_VertexBuffer.h"
 #include "../RHI/RHI_DescriptorCache.h"
 #include "../RHI/RHI_Implementation.h"
+#include "../Display/Display.h"
 //=========================================
 
 //= NAMESPACES ===============
