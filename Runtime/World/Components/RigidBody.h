bd4ce898585f4c4da0eadf029efbecf330d85ed7
Odyssey <47780708+OdysseyGuy@users.noreply.github.com>
2020-10-19 05:25:55 -0700

    Update RigidBody.h

--- Runtime/World/Components/RigidBody.h
+++ Runtime/World/Components/RigidBody.h
@@ -24,7 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //= INCLUDES ==================
 #include "IComponent.h"
 #include <vector>
-#include "../../Math/Vector3.h"
+#include "Math/Vector3.h"
 //=============================
 
 class btRigidBody;
