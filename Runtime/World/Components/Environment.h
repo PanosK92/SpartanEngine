d8ad6e9de83405bafe4b69685252bc59f9f598fd
Odyssey <47780708+OdysseyGuy@users.noreply.github.com>
2020-10-19 05:24:35 -0700

    Update Environment.h

--- Runtime/World/Components/Environment.h
+++ Runtime/World/Components/Environment.h
@@ -23,7 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 //= INCLUDES ========================
 #include "IComponent.h"
-#include "../../RHI/RHI_Definition.h"
+#include "RHI/RHI_Definition.h"
 //===================================
 
 namespace Spartan
