6136100b07c1e606ce8feb439bb732a1728be134
Panos Karabelas <PanosConroe@hotmail.com>
2021-02-23 18:05:13 +0000

    [Editor] Added missing icon

--- Runtime/RHI/Vulkan/Vulkan_BlendState.cpp
+++ Runtime/RHI/Vulkan/Vulkan_BlendState.cpp
@@ -37,21 +37,21 @@ namespace Spartan
         const std::shared_ptr<RHI_Device>& device,
         const bool blend_enabled                    /*= false*/,
         const RHI_Blend source_blend                /*= Blend_Src_Alpha*/,
-        const RHI_Blend dest_blend                    /*= Blend_Inv_Src_Alpha*/,
-        const RHI_Blend_Operation blend_op            /*= Blend_Operation_Add*/,
-        const RHI_Blend source_blend_alpha            /*= Blend_One*/,
+        const RHI_Blend dest_blend                  /*= Blend_Inv_Src_Alpha*/,
+        const RHI_Blend_Operation blend_op          /*= Blend_Operation_Add*/,
+        const RHI_Blend source_blend_alpha          /*= Blend_One*/,
         const RHI_Blend dest_blend_alpha            /*= Blend_One*/,
-        const RHI_Blend_Operation blend_op_alpha,    /*= Blend_Operation_Add*/
+        const RHI_Blend_Operation blend_op_alpha,   /*= Blend_Operation_Add*/
         const float blend_factor                    /*= 0.0f*/
     )
     {
         // Save parameters
-        m_blend_enabled            = blend_enabled;
-        m_source_blend            = source_blend;
+        m_blend_enabled          = blend_enabled;
+        m_source_blend          = source_blend;
         m_dest_blend            = dest_blend;
-        m_blend_op                = blend_op;
+        m_blend_op              = blend_op;
         m_source_blend_alpha    = source_blend_alpha;
-        m_dest_blend_alpha        = dest_blend_alpha;
+        m_dest_blend_alpha      = dest_blend_alpha;
         m_blend_op_alpha        = blend_op_alpha;
         m_blend_factor          = blend_factor;
     }
