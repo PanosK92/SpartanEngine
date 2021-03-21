c47ea7e2f14973ff1bdc59689b7aae70384f9117
Panos Karabelas <PanosConroe@hotmail.com>
2021-03-21 16:00:29 +0000

    [Scripts] Fixed an issue where clean.sh would not clean the binary directories

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -4,7 +4,11 @@
 rm -rf Binaries/intermediate
 
 # delete binary directory files (that don't need to be part of build artifacts)
-rm -f Binaries/*.exp
-rm -f Binaries/*.ilk
-rm -f Binaries/*.lib
-rm -f Binaries/*.pdb
+rm -f Binaries/Debug/*.exp
+rm -f Binaries/Debug/*.ilk
+rm -f Binaries/Debug/*.lib
+rm -f Binaries/Debug/*.pdb
+rm -f Binaries/Release/*.exp
+rm -f Binaries/Release/*.ilk
+rm -f Binaries/Release/*.lib
+rm -f Binaries/Release/*.pdb
