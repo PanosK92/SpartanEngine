c9aa865cb32b9b764f61bcc09af74db8caabef3f
Panos Karabelas <PanosConroe@hotmail.com>
2021-03-21 15:47:40 +0000

    [Scripts] Replaced batch with bash scripts

--- Generate_VS2019_D3D11.bat
+++ Generate_VS2019_D3D11.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /D "%~dp0"
-call "Scripts\generate_project_files.bat" vs2019 d3d11
+Scripts\win-bash\bash.exe ./Scripts/generate_project_files.sh vs2019 d3d11
 exit
--- Scripts/generate_project_files.sh
+++ Scripts/generate_project_files.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
-echo "1. Deleting intermediate folder and lib files (from the binary directory)..."
+# Deduce OS we are running on
+is_windows=false
+if [ "$1" = "vs2019" ]; then
+    is_windows=true
+fi
+
+# Delete intermediate directory (obj files)
+echo "1. Deleting intermediate directory and uncessery files from the binary directory..."
 ./Scripts/clean.sh
 
-echo "2. Extracting third-party dependencies..."
-./Scripts/7za e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+# Extract third-party libraries (that the project will link to)
+echo
+echo "= 2. Extracting third-party dependencies... ==========================================="
+if [ "$is_windows" = false ]; then
+	./Scripts/7za e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+else
+	./Scripts/7z.exe e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+fi
+echo "======================================================================================="
 
+# Copy engine data to the binary directory
+echo
 echo "3. Copying required data to the binary directory..."
-mkdir -p Binaries/Debug/Data
-cp -R Data Binaries/Debug/Data
-mkdir -p Binaries/Release/Data
-cp -R Data Binaries/Release/Data
+cp -r Data Binaries/Debug/Data
+cp -r Data Binaries/Release/Data
 
+# Copy engine DLLs to the binary directory
+echo
 echo "4. Copying required DLLs to the binary directory..."
 cp ThirdParty/libraries/dxcompiler.dll Binaries/Debug/
 cp ThirdParty/libraries/fmodL64.dll	Binaries/Debug/
 cp ThirdParty/libraries/dxcompiler.dll Binaries/Release/
 cp ThirdParty/libraries/fmod64.dll Binaries/Release/
 
-echo "5. Generating MakeFiles..."
+# Generate project files
+echo
+if [ "$is_windows" = false ]; then
+	echo "5. Generating MakeFiles..."
+else
+	echo "5. Generating Visual Studio solution..."
+fi
+echo
 ./Scripts/premake5 --file=Scripts/premake.lua $@
