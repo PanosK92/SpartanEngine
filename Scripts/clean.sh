ac975d4ecd96544af7ab6177e8dd1d711d2c5f63
Panos Karabelas <panosconroe@hotmail.com>
2021-06-27 22:08:16 +0100

    [Build] Further simplified things by placing both the exe and obj files under the Binaries directory (obj files are deleted before creating an artifact zip file)

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -4,7 +4,10 @@
 rm -rf Binaries/Obj
 
 # delete exe directory files (that don't need to be part of build artifacts)
-rm -f Binaries/Exe/*.exp
-rm -f Binaries/Exe/*.ilk
-rm -f Binaries/Exe/*.lib
-rm -f Binaries/Exe/*.pdb
+rm -f Binaries/*.exp
+rm -f Binaries/*.ilk
+rm -f Binaries/*.lib
+rm -f Binaries/*.pdb
+
+# delete debug dlls (in case they exist)
+rm -rf Binaries/fmodL64.dll
