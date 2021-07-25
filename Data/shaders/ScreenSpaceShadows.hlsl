68be82a65639b9c2781a48fcc4b87c07170607e9
Panos Karabelas <panosconroe@hotmail.com>
2021-07-25 23:44:21 +0100

    [ScreenSpaceShadows] Reduced artifacts

--- Data/shaders/ScreenSpaceShadows.hlsl
+++ Data/shaders/ScreenSpaceShadows.hlsl
@@ -20,10 +20,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 // Settings
-static const uint  g_sss_max_steps        = 12;  // Max ray steps, affects quality and performance.
-static const float g_sss_ray_max_distance = 0.05f;  // Max shadow length, longer shadows are less accurate.
-static const float g_sss_thickness        = 0.05f;  // Depth testing thickness.
-static const float g_sss_step_length      = g_sss_ray_max_distance / (float)g_sss_max_steps;
+static const uint  g_sss_max_steps                     = 12;    // Max ray steps, affects quality and performance.
+static const float g_sss_ray_max_distance              = 0.05f; // Max shadow length, longer shadows are less accurate.
+static const float g_sss_thickness                     = 0.05f; // Depth testing thickness.
+static const float g_sss_max_delta_from_original_depth = 0.01f; // The maximum allowed depth deviation from the original pixel (a big deviation decreased the probabilty that the pixel is the occluder).
+static const float g_sss_step_length                   = g_sss_ray_max_distance / (float)g_sss_max_steps;
 
 //= INLUCES ==========
 #include "Common.hlsl"
@@ -41,7 +42,10 @@ float ScreenSpaceShadows(Surface surface, Light light)
     // Offset starting position with temporal interleaved gradient noise
     float offset = get_noise_interleaved_gradient(surface.uv * g_resolution_rt);
     ray_pos      += ray_step * offset;
-
+    
+    // Save the original depth
+    float depth_original = ray_pos.z;
+    
     // Ray march towards the light
     float occlusion = 0.0;
     float2 ray_uv   = 0.0f;
@@ -52,23 +56,24 @@ float ScreenSpaceShadows(Surface surface, Light light)
         ray_uv  = view_to_uv(ray_pos);
 
         // Ensure the UV coordinates are inside the screen
-        if (is_saturated(ray_uv))
+        if (!is_saturated(ray_uv))
+            return 1.0f;
+        
+        // Compute the difference between the ray's and the camera's depth
+        float depth_z       = get_linear_depth(ray_uv);
+        float depth_delta   = ray_pos.z - depth_z;
+
+        bool can_the_camera_see_the_ray     = (depth_delta > 0.0f) && (depth_delta < g_sss_thickness);
+        bool occluded_by_the_original_pixel = abs(ray_pos.z - depth_original) < g_sss_max_delta_from_original_depth;
+        if (can_the_camera_see_the_ray && occluded_by_the_original_pixel)
         {
-            // Compute the difference between the ray's and the camera's depth
-            float depth_z       = get_linear_depth(ray_uv);
-            float depth_delta   = ray_pos.z - depth_z;
+            // Mark as occluded
+            occlusion = 1.0f;
 
-            // Check if the camera can't "see" the ray (ray depth must be larger than the camera depth, so positive depth_delta)
-            if ((depth_delta > 0.0f) && (depth_delta < g_sss_thickness))
-            {
-                // Mark as occluded
-                occlusion = 1.0f;
+            // Fade out as we approach the edges of the screen
+            occlusion *= screen_fade(ray_uv);
 
-                // Fade out as we approach the edges of the screen
-                occlusion *= screen_fade(ray_uv);
-
-                break;
-            }
+            break;
         }
     }
 
