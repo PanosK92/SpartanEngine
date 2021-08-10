e85a97f8714a9f11fc9fdd8853d8bb620cade38d
Panos Karabelas <panosconroe@hotmail.com>
2021-08-10 23:04:21 +0100

    [IBL] Finalised blending between SSR and IBL, the key was that the SSR really only needs to blend with the IBL specular (in case no hits exist) and then added to the IBL diffuse which is always done. Looks good.

--- Data/shaders/light_image_based.hlsl
+++ Data/shaders/light_image_based.hlsl
@@ -23,6 +23,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "brdf.hlsl"
 //==================
 
+static const float g_ssr_fallback_threshold_roughness = 0.7f; // value above which blending with the environment is forced
+
 // From Sebastien Lagarde Moving Frostbite to PBR page 69
 float3 get_dominant_specular_direction(float3 normal, float3 reflection, float roughness)
 {
@@ -69,36 +71,38 @@ float4 mainPS(Pixel_PosUv input) : SV_TARGET
     const float3 F               = F_Schlick_Roughness(surface.F0, n_dot_v, surface.roughness);
     const float2 envBRDF         = tex_lutIbl.SampleLevel(sampler_bilinear_clamp, float2(n_dot_v, surface.roughness), 0.0f).xy;
     const float3 specular_energy = F * envBRDF.x + envBRDF.y;
-    
+
+    // IBL - Diffuse
+    float3 diffuse_energy = compute_diffuse_energy(specular_energy, surface.metallic); // Used to town down diffuse such as that only non metals have it
+    float3 light_ambient  = saturate(g_directional_light_intensity / 128000.0f) * surface.occlusion; // just a hack
+    float3 ibl_diffuse    = sample_environment(direction_sphere_uv(surface.normal), g_envrionement_max_mip) * surface.albedo.rgb * light_ambient * diffuse_energy;
+    ibl_diffuse           *= surface.alpha; // Fade out for transparents
+
     // Get ssr color
     float mip_level         = lerp(0, g_ssr_mip_count, surface.roughness);
     const float4 ssr_sample = (is_ssr_enabled() && !g_is_transparent_pass) ? tex_ssr.SampleLevel(sampler_trilinear_clamp, surface.uv, mip_level) : 0.0f;
-    const float3 color_ssr  = ssr_sample.rgb;
-    const float ssr_alpha   = ssr_sample.a * luminance(specular_energy);
+    const float3 color_ssr  = ssr_sample.rgb ;
+    float ssr_alpha         = ssr_sample.a;
 
-    // Get environment color
-    float3 color_environment = 0.0f;
-    if (ssr_alpha != 1.0f)
+    // Remap alpha above a certain roughness threshold in order to hide blocky reflections (from very small mips)
+    if (surface.roughness > g_ssr_fallback_threshold_roughness)
     {
-        float3 light_ambient = saturate(g_directional_light_intensity / 128000.0f) * surface.occlusion;
-        if (any(light_ambient))
-        {
-            // Specular
-            {
-                const float3 reflection            = reflect(surface.camera_to_pixel, surface.normal);
-                float3 dominant_specular_direction = get_dominant_specular_direction(surface.normal, reflection, surface.roughness);
-                float mip_level                    = lerp(0, g_envrionement_max_mip, surface.roughness);
-                color_environment                  = sample_environment(direction_sphere_uv(dominant_specular_direction), mip_level) * specular_energy;
-            }
-
-            // Diffuse
-            float3 diffuse_energy = compute_diffuse_energy(specular_energy, surface.metallic); // Used to town down diffuse such as that only non metals have it
-            color_environment     += sample_environment(direction_sphere_uv(surface.normal), g_envrionement_max_mip) * surface.albedo.rgb * light_ambient * diffuse_energy; 
-
-            // Fade out for transparents
-            color_environment *= surface.alpha;
-        }
+        ssr_alpha = lerp(ssr_alpha, 0.0f, (surface.roughness - g_ssr_fallback_threshold_roughness) / (1.0f - g_ssr_fallback_threshold_roughness));
+    }
+    
+    // IBL - Specular
+    float3 ibl_specular_environment = 0.0f;
+    if (ssr_alpha != 1.0f && any(light_ambient))
+    {
+        const float3 reflection            = reflect(surface.camera_to_pixel, surface.normal);
+        float3 dominant_specular_direction = get_dominant_specular_direction(surface.normal, reflection, surface.roughness);
+        float mip_level                    = lerp(0, g_envrionement_max_mip, surface.roughness);
+        ibl_specular_environment           = sample_environment(direction_sphere_uv(dominant_specular_direction), mip_level) ;
     }
 
-    return float4(saturate_11(lerp(color_environment, color_ssr, ssr_alpha)), 0.0f);
+    // Blend between speculars and account for the specular energy
+    float3 ibl_specular = lerp(ibl_specular_environment, color_ssr, ssr_alpha) * specular_energy;
+
+    // Perfection achieved
+    return float4(saturate_11(ibl_diffuse + ibl_specular), 0.0f);
 }
