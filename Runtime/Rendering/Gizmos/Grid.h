73b54f9f9a3ced9e6b772ad329f007e084c1c490
Panos Karabelas <panosconroe@hotmail.com>
2021-10-10 18:12:01 +0100

    [Renderer] Removed redundant index buffer from world grid rendering.

--- Runtime/Rendering/Gizmos/Grid.h
+++ Runtime/Rendering/Gizmos/Grid.h
@@ -41,20 +41,17 @@ namespace Spartan
         ~Grid() = default;
         
         const Math::Matrix& ComputeWorldMatrix(Transform* camera);
-        
-        const auto& GetIndexBuffer() const  { return m_indexBuffer; }
-        const auto& GetVertexBuffer() const { return m_vertexBuffer; }
-        uint32_t GetIndexCount() const      { return m_indexCount; }
+        const auto& GetVertexBuffer()   const { return m_vertex_buffer; }
+        const uint32_t GetVertexCount() const { return m_vertex_count; }
 
     private:
-        void BuildGrid(std::vector<RHI_Vertex_PosCol>* vertices, std::vector<uint32_t>* indices);
-        bool CreateBuffers(std::vector<RHI_Vertex_PosCol>& vertices, std::vector<uint32_t>& indices, std::shared_ptr<RHI_Device>& rhi_device);
+        void BuildGrid(std::vector<RHI_Vertex_PosCol>* vertices);
 
-        uint32_t m_indexCount;
-        uint32_t m_terrainHeight;
-        uint32_t m_terrainWidth;
-        std::shared_ptr<RHI_VertexBuffer> m_vertexBuffer;
-        std::shared_ptr<RHI_IndexBuffer> m_indexBuffer;
+        uint32_t m_terrain_height = 200;
+        uint32_t m_terrain_width  = 200;
+        uint32_t m_vertex_count   = 0;
+
+        std::shared_ptr<RHI_VertexBuffer> m_vertex_buffer;
         Math::Matrix m_world;
     };
 }
