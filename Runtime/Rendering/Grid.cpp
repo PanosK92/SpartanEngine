6c4c14aa0958309faa7496b7ff9d064053e3cacd
Panos Karabelas <panosconroe@hotmail.com>
2021-12-31 20:28:05 +0000

    [Build] Fixed another error reported by the CI system (can't see them localy for some reason)

--- Runtime/Rendering/Grid.cpp
+++ Runtime/Rendering/Grid.cpp
@@ -19,13 +19,13 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-//= INCLUDES ================================
+//= INCLUDES =============================
 #include "Spartan.h"
 #include "Grid.h"
-#include "../../World/Components/Transform.h"
-#include "../../RHI/RHI_VertexBuffer.h"
-#include "../../RHI/RHI_Vertex.h"
-//===========================================
+#include "../RHI/RHI_Vertex.h"
+#include "../RHI/RHI_VertexBuffer.h"
+#include "../World/Components/Transform.h"
+//========================================
 
 //= NAMESPACES ================
 using namespace std;
