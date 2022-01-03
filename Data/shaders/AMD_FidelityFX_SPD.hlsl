93137af01c82d184408039b402ae3a0bd6f6be19
Panos Karabelas <panosconroe@hotmail.com>
2022-01-03 23:10:39 +0000

    [Shaders] Fixed even more warnings

--- Data/shaders/AMD_FidelityFX_SPD.hlsl
+++ Data/shaders/AMD_FidelityFX_SPD.hlsl
@@ -1,5 +1,5 @@
 /*
-Copyright(c) 2016-2021 Panos Karabelas
+Copyright(c) 2016-2022 Panos Karabelas
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 //= INCLUDES =========
-#include "Common.hlsl"
+#include "common.hlsl"
 //====================
 
 #define A_GPU
