1050d11fd396be7a5372b2fb80a9bb3b536dd97e
Panos Karabelas <panosconroe@hotmail.com>
2022-06-09 21:34:55 +0100

    [CI] Even more improvements

--- build_scripts/generate_project_files.sh
+++ build_scripts/generate_project_files.sh
@@ -10,24 +10,24 @@ fi
 echo
 echo "= 1. Extracting third-party dependencies... ==========================================="
 if [ "$is_windows" = false ]; then
-	 Scripts/7za e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+	 build_scripts/7za e third_party/libraries/libraries.7z -othird_party/libraries/ -aoa
 else 
-	 Scripts/7z.exe e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+	 build_scripts/7z.exe e third_party/libraries/libraries.7z -othird_party/libraries/ -aoa
 fi
 echo "======================================================================================="
 
 # Copy engine data to the binary directory
 echo
 echo "2. Copying required data to the binary directory..."
-mkdir -p Binaries/
-cp -r Data Binaries
+mkdir -p binaries/
+cp -r Data binaries
 
 # Copy engine DLLs to the binary directory
 echo
 echo "3. Copying required DLLs to the binary directory..."
-cp ThirdParty/libraries/dxcompiler.dll Binaries/
-cp ThirdParty/libraries/fmod64.dll Binaries/
-cp ThirdParty/libraries/fmodL64.dll Binaries/
+cp third_party/libraries/dxcompiler.dll Binaries/
+cp third_party/libraries/fmod64.dll Binaries/
+cp third_party/libraries/fmodL64.dll Binaries/
 
 # Generate project files
 echo
@@ -37,4 +37,4 @@ else
 	echo "4. Generating Visual Studio solution..."
 fi
 echo
-Scripts/premake5 --file=Scripts/premake.lua $@
+build_scripts/premake5 --file=build_scripts/premake.lua $@
--- scripts/package_artifact.bat
+++
@@ -1,1 +0,0 @@
-scripts\7z.exe a build_%1.7z binaries\dxcompiler.dll binaries\data binaries\spartan_%1.exe
