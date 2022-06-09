98e1ab8137731f70ccb43b89263c12c76b8b4c0e
Panos Karabelas <panosconroe@hotmail.com>
2022-06-09 19:51:04 +0100

    [CI] Various script improvements

--- Generate_VS2022_Vulkan.bat
+++ Generate_VS2022_Vulkan.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /D "%~dp0"
-Scripts\win-bash\bash.exe ./Scripts/generate_project_files.sh vs2022 vulkan
+Scripts\win-bash\bash.exe scripts/generate_project_files.sh vs2022 vulkan
 exit
--- Scripts/generate_project_files.sh
+++ Scripts/generate_project_files.sh
@@ -6,39 +6,35 @@ if [ "$1" != "gmake2" ]; then
     is_windows=true
 fi
 
-# Delete any files which are not needed for a CI artifact to be created
-echo "1. Deleting uncessery files from the binary directory..."
-./Scripts/clean.sh
-
 # Extract third-party libraries (that the project will link to)
 echo
-echo "= 2. Extracting third-party dependencies... ==========================================="
+echo "= 1. Extracting third-party dependencies... ==========================================="
 if [ "$is_windows" = false ]; then
-	./Scripts/7za e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
-else
-	./Scripts/7z.exe e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+	 Scripts/7za e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+else 
+	 Scripts/7z.exe e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
 fi
 echo "======================================================================================="
 
 # Copy engine data to the binary directory
 echo
-echo "3. Copying required data to the binary directory..."
+echo "2. Copying required data to the binary directory..."
 mkdir -p Binaries/
 cp -r Data Binaries
 
 # Copy engine DLLs to the binary directory
 echo
-echo "4. Copying required DLLs to the binary directory..."
+echo "3. Copying required DLLs to the binary directory..."
 cp ThirdParty/libraries/dxcompiler.dll Binaries/
 cp ThirdParty/libraries/fmod64.dll Binaries/
-cp ThirdParty/libraries/fmodL64.dll	Binaries/
+cp ThirdParty/libraries/fmodL64.dll Binaries/
 
 # Generate project files
 echo
 if [ "$is_windows" = false ]; then
-	echo "5. Generating MakeFiles..."
+	echo "4. Generating MakeFiles..."
 else
-	echo "5. Generating Visual Studio solution..."
+	echo "4. Generating Visual Studio solution..."
 fi
 echo
-./Scripts/premake5 --file=Scripts/premake.lua $@
+Scripts/premake5 --file=Scripts/premake.lua $@
