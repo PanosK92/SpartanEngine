6f286cfa02e2f15255a9246a64284c4fa8afa56d
Panos Karabelas <panosconroe@hotmail.com>
2022-06-09 22:33:15 +0100

    Renamed third party folder

--- build_scripts/generate_project_files.sh
+++ build_scripts/generate_project_files.sh
@@ -10,24 +10,24 @@ fi
 echo
 echo "= 1. Extracting third-party dependencies... ==========================================="
 if [ "$is_windows" = false ]; then
-	 build_scripts/7za e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+	 build_scripts/7za e third_party/libraries/libraries.7z -othird_party/libraries/ -aoa
 else 
-	 build_scripts/7z.exe e ThirdParty/libraries/libraries.7z -oThirdParty/libraries/ -aoa
+	 build_scripts/7z.exe e third_party/libraries/libraries.7z -othird_party/libraries/ -aoa
 fi
 echo "======================================================================================="
 
 # Copy engine data to the binary directory
 echo
 echo "2. Copying required data to the binary directory..."
-mkdir -p Binaries/
-cp -r Data Binaries
+mkdir -p binaries/
+cp -r Data binaries
 
 # Copy engine DLLs to the binary directory
 echo
 echo "3. Copying required DLLs to the binary directory..."
-cp ThirdParty/libraries/dxcompiler.dll Binaries/
-cp ThirdParty/libraries/fmod64.dll Binaries/
-cp ThirdParty/libraries/fmodL64.dll Binaries/
+cp third_party/libraries/dxcompiler.dll Binaries/
+cp third_party/libraries/fmod64.dll Binaries/
+cp third_party/libraries/fmodL64.dll Binaries/
 
 # Generate project files
 echo
