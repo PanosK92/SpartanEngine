a7bf79644302a1f78e518f07af6bce98c894bdfb
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-06-22 13:32:19 +0200

    ifdefs for gcc specific compiler intrinsics (__debugbreak, __declspec)

--- Runtime/Core/SpartanDefinitions.h
+++ Runtime/Core/SpartanDefinitions.h
@@ -1,28 +1,56 @@
 #pragma once
 
+// TODO: see a way to add #pragma GCC diagnostic push/pop with macros (maybe something like _Pragma)
+
 // Version
 #define sp_version "v0.33"
 
-// Class
-#define SPARTAN_CLASS
-#if SPARTAN_RUNTIME_SHARED == 1
-#ifdef SPARTAN_RUNTIME
-#define SPARTAN_CLASS __declspec(dllexport)
+#if defined(__GNUC__)
+    // Class
+    #define SPARTAN_CLASS
+    #if SPARTAN_RUNTIME_SHARED == 1
+        #ifdef SPARTAN_RUNTIME
+            #define SPARTAN_CLASS __attribute__((visibility("default")))
+        #else
+            #define SPARTAN_CLASS 
+        #endif
+    #endif
+
+    // Optimisation
+    #define SP_OPTIMISE_OFF
+    #define SP_OPTIMISE_ON
+
+    // Warnings
+    #define SP_WARNINGS_OFF
+    #define SP_WARNINGS_ON
+
+    // Debug break
+    #define SP_DEBUG_BREAK() abort()
+#elif defined(_MSC_VER)
+    // Class
+    #define SPARTAN_CLASS
+    #if SPARTAN_RUNTIME_SHARED == 1
+        #ifdef SPARTAN_RUNTIME
+            #define SPARTAN_CLASS __declspec(dllexport)
+        #else
+            #define SPARTAN_CLASS __declspec(dllimport)
+        #endif
+    #endif
+
+    // Optimisation
+    #define SP_OPTIMISE_OFF __pragma(optimize("", off))
+    #define SP_OPTIMISE_ON __pragma(optimize("", on))
+
+    // Warnings
+    #define SP_WARNINGS_OFF __pragma(warning(push, 0))
+    #define SP_WARNINGS_ON __pragma(warning(pop))
+
+    // Debug break
+    #define SP_DEBUG_BREAK() __debugbreak()
 #else
-#define SPARTAN_CLASS __declspec(dllimport)
+    #pragma warn "Unkown compiler/platform."
 #endif
-#endif
 
-// Optimisation
-#define SP_OPTIMISE_OFF __pragma(optimize("", off))
-#define SP_OPTIMISE_ON __pragma(optimize("", on))
-
-// Warnings
-#define SP_WARNINGS_OFF __pragma(warning(push, 0))
-#define SP_WARNINGS_ON __pragma(warning(pop))
-
-// Debug break
-#define SP_DEBUG_BREAK() __debugbreak()
 
 //= Assert ==========================================================================
 // On debug mode, the assert will have the default behaviour.
@@ -62,7 +90,7 @@ if (x)               \
 //= DISABLED WARNINGS ==============================================================================================================================
 // identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
 #pragma warning(disable: 4251) // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=vs-2019
-// non – DLL-interface classkey 'identifier' used as base for DLL-interface classkey 'identifier'
+// non ï¿½ DLL-interface classkey 'identifier' used as base for DLL-interface classkey 'identifier'
 #pragma warning(disable: 4275) // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=vs-2019
 // no definition for inline function 'function'
 #pragma warning(disable: 4506) // https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4506?view=vs-2017
