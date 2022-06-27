7d62edfaaab554faebabfe5eac4c8abc518ddb13
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-06-27 18:57:39 +0200

    [Editor] Using WinMain on windows and traditional main on other systems

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -24,7 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <Windows.h>
 //==================
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int main(int argc, char** argv)
 {
     Editor editor;
     editor.Tick();
