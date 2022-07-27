57dba9492311702e1627d8cd761f686680e4ae99
Panos Karabelas <panosconroe@hotmail.com>
2022-07-27 21:07:47 +0100

    [Cleanup] Fixed some warnings coming

--- Runtime/RHI/RHI_PhysicalDevice.h
+++ Runtime/RHI/RHI_PhysicalDevice.h
@@ -94,47 +94,7 @@ namespace Spartan
             return name;
         }
 
-        std::string decode_driver_version(const uint32_t version)
-        {
-            char buffer[256];
-            
-            if (IsNvidia())
-            {
-                sprintf
-                (
-                    buffer,
-                    "%d.%d.%d.%d",
-                    (version >> 22) & 0x3ff,
-                    (version >> 14) & 0x0ff,
-                    (version >> 6) & 0x0ff,
-                    (version) & 0x003f
-                );
-                
-            }
-            else if(IsIntel())
-            {
-                sprintf
-                (
-                    buffer,
-                    "%d.%d",
-                    (version >> 14),
-                    (version) & 0x3fff
-                );
-            }
-            else // Use Vulkan version conventions if vendor mapping is not available
-            {
-                sprintf
-                (
-                    buffer,
-                    "%d.%d.%d",
-                    (version >> 22),
-                    (version >> 12) & 0x3ff,
-                    version & 0xfff
-                );
-            }
-
-            return buffer;
-        }
+        std::string decode_driver_version(const uint32_t version);
 
         std::string api_version      = "Unknown"; // version of api supported by the device
         std::string driver_version   = "Unknown"; // vendor-specified version of the driver.
