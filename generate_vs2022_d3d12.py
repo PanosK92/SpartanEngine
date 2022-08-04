333ab1ab3af4a09fed131a58724a62a101e45ecf
Panos Karabelas <PanosConroe@hotmail.com>
2022-08-04 13:26:21 +0100

    Update generate_vs2022_d3d12.py

--- generate_vs2022_d3d12.py
+++ generate_vs2022_d3d12.py
@@ -4,6 +4,6 @@ import sys
 # change working directory to script directory
 os.chdir(os.path.dirname(__file__))
 # run script
-subprocess.Popen("build_scripts\\generate_project_files.py vs2022 d3d12", shell=True).communicate()
+subprocess.Popen("python build_scripts\\generate_project_files.py vs2022 d3d12").communicate()
 # exit
 sys.exit(0)
