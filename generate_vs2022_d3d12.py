eb2d2fa28ee81e4e7a94828a017af1eb5c95a94f
Panos Karabelas <panosconroe@hotmail.com>
2022-08-04 03:24:19 +0100

    [Build] Updated the projection generation script to start the premake lua script as as subproccess

--- generate_vs2022_d3d12.py
+++ generate_vs2022_d3d12.py
@@ -3,5 +3,6 @@ import subprocess
 # change working directory to script directory
 os.chdir(os.path.dirname(__file__))
 # run script
-p = subprocess.Popen("build_scripts\\generate_project_files.py vs2022 d3d12", shell=True)
-p.communicate() # wait for script to finish
+subprocess.Popen("build_scripts\\generate_project_files.py vs2022 d3d12", shell=True).communicate()
+# exit
+sys.exit(0)
