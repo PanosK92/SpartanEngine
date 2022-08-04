52c0eeea32270759cea953704dbe252e076b4df4
Panos Karabelas <panosconroe@hotmail.com>
2022-08-04 03:14:55 +0100

    [Builds] Tweaked scripts to lunch the project generation python script, as a subprocess, wth a pipe and wait for execution to finish

--- generate_vs2022_d3d11.py
+++ generate_vs2022_d3d11.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 # change working directory to script directory
 os.chdir(os.path.dirname(__file__))
 # run script
-os.system("build_scripts\\generate_project_files.py vs2022 d3d11")
+p = subprocess.Popen("build_scripts\\generate_project_files.py vs2022 d3d11", shell=True)
+p.communicate() # wait for script to finish
