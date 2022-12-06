bd99648f2807dc9375d6de8270ea6b8108934db4
Panos Karabelas <panosconroe@hotmail.com>
2022-12-06 14:08:08 +0000

    [Editor] The startup load world window prompt has been updated to contain a list of possible options

--- runtime/World/World.h
+++ runtime/World/World.h
@@ -57,7 +57,8 @@ namespace Spartan
         bool SaveToFile(const std::string& filePath);
         bool LoadFromFile(const std::string& file_path);
         void Resolve() { m_resolve = true; }
-        void CreateDefaultWorld();
+        void CreateDefaultWorldSimple();
+        void CreateDefaultWorldComplex();
         const std::string GetName()      const { return m_name; }
         const std::string& GetFilePath() const { return m_file_path; }
 
