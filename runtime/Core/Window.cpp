fe0d2b96ee8149052090f70f5ac606442580be7a
mamingkun <mamingkun3@gmail.com>
2023-03-02 00:04:13 +0800

    1. commit test

--- runtime/Core/Window.cpp
+++ runtime/Core/Window.cpp
@@ -310,6 +310,7 @@ namespace Spartan
         SDL_VERSION(&sys.version);
         SDL_GetWindowWMInfo(m_window, &sys);
         return sys.info.win.window;
+
 #else
         return m_window;
 #endif
