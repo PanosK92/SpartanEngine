27ddc1244ccbd0f5b01ed788e07702b28e8e2a4a
Panos Karabelas <panosconroe@hotmail.com>
2023-04-02 09:11:12 +0100

    [Shaders] Some parameter adjustments

--- data/shaders/ssr.hlsl
+++ data/shaders/ssr.hlsl
@@ -24,7 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //====================
 
 static const float g_ssr_max_distance       = 100.0f;
-static const uint g_ssr_max_steps           = 64;
+static const uint g_ssr_max_steps           = 45;
 static const uint g_ssr_binary_search_steps = 24;
 static const float g_ssr_thickness          = 0.0001f;
 
@@ -50,63 +50,9 @@ bool intersect_depth_buffer(float2 ray_pos, float2 ray_start, float ray_length, 
 {
     float depth_ray  = get_depth_from_ray(ray_pos, ray_start, ray_length, z_start, z_end);
     float depth_real = get_linear_depth(ray_pos);
-    depth_delta      = depth_ray - depth_real;
+    depth_delta      = (depth_ray - depth_real);
 
     return depth_delta >= 0.0f;
-}
-
-bool lines_interesect(float2 a1, float2 a2, float2 b1, float2 b2, out float2 intersection)
-{
-    intersection = 0.0f;
-
-    float2 b = a2 - a1;
-    float2 d = b2 - b1;
-    float bDotDPerp = b.x * d.y - b.y * d.x;
-
-    // if b dot d == 0, it means the lines are parallel so have infinite intersection points
-    if (bDotDPerp == 0)
-        return false;
-
-    float2 c = b1 - a1;
-    float t = (c.x * d.y - c.y * d.x) / bDotDPerp;
-    if (t < 0 || t > 1)
-        return false;
-
-    float u = (c.x * b.y - c.y * b.x) / bDotDPerp;
-    if (u < 0 || u > 1)
-        return false;
-
-    intersection = a1 + t * b;
-
-    return true;
-}
-
-float2 clip_uv_ray_end_to_bounds(const float2 ray_start, const float2 ray_end)
-{
-    float2 top_left     = float2(0.0f, 0.0f);
-    float2 top_right    = float2(1.0f, 0.0f);
-    float2 bottom_right = float2(1.0f, 1.0f);
-    float2 bottom_left  = float2(0.0f, 1.0f);
-
-    float2 intersection = 0.0f;
-
-    // Top
-    if (lines_interesect(ray_start, ray_end, top_left, top_right, intersection))
-        return intersection;
-
-    // Right
-    if (lines_interesect(ray_start, ray_end, top_right, bottom_right, intersection))
-        return intersection;
-
-    // Bottom
-    if (lines_interesect(ray_start, ray_end, bottom_right, bottom_left, intersection))
-        return intersection;
-
-    // Left
-    if (lines_interesect(ray_start, ray_end, bottom_left, top_left, intersection))
-        return intersection;
-        
-    return false;
 }
 
 float2 trace_ray(uint2 screen_pos, float3 ray_start_vs, float3 ray_dir_vs)
@@ -120,15 +66,10 @@ float2 trace_ray(uint2 screen_pos, float3 ray_start_vs, float3 ray_dir_vs)
     float2 ray_start = view_to_uv(ray_start_vs);
     float2 ray_end   = view_to_uv(ray_end_vs);
 
-    // Clip ray end to uv bounds
-    //float2 ray_end_clipped = clip_uv_ray_end_to_bounds(ray_start, ray_end); 
-    //depth_end              = get_depth_from_ray(ray_end_clipped, ray_start, length(ray_end_clipped - ray_start), depth_start, depth_end);
-    //ray_end                = ray_end_clipped;
-
     // Compute ray step
     float2 ray_start_to_end = ray_end - ray_start;
     float ray_length        = length(ray_start_to_end);
-    float2 ray_step         = (ray_start_to_end) / (float)g_ssr_max_steps;
+    float2 ray_step         =  (ray_start_to_end + FLT_MIN) / (float)(g_ssr_max_steps);
     float2 ray_pos          = ray_start;
 
     // Adjust position with some temporal noise (TAA will do some magic later)
@@ -204,13 +145,13 @@ void mainCS(uint3 thread_id : SV_DispatchThreadID)
     float2 hit_uv = -1.0f;
 
     // Compute reflection direction in view space
-    float3 normal          = get_normal_view_space(thread_id.xy);
-    float3 position        = get_position_view_space(thread_id.xy);
+    float3 normal          = world_to_view(surface.normal, false);
+    float3 position        = world_to_view(surface.position, true);
     float3 camera_to_pixel = normalize(position);
     float3 reflection      = normalize(reflect(camera_to_pixel, normal));
     float v_dot_r          = dot(-camera_to_pixel, reflection);
 
-    // Don't trace rays which facing the camera
+    // Don't trace rays which are facing the camera
     if (v_dot_r < 0.0f)
     { 
         hit_uv = trace_ray(thread_id.xy, position, reflection);
@@ -225,4 +166,3 @@ void mainCS(uint3 thread_id : SV_DispatchThreadID)
 
     tex_uav[thread_id.xy] = float4(color, alpha);
 }
-
