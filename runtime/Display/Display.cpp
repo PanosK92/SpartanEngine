49392ea69490992b854622bce20ed16b57a94f18
N. Pattakos <pattakosn@fastmail.com>
2023-07-07 19:03:01 +0200

    project's header can be in double quotes as per MR comments

--- runtime/Display/Display.cpp
+++ runtime/Display/Display.cpp
@@ -23,7 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "pch.h"
 #include "Display.h"
 #include <SDL.h>
-#include <Window.h>
+#include "Window.h"
 //==================
 
 #if defined(_MSC_VER)
