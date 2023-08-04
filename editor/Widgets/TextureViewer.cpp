8ace621ce6d9c728bd6d9838408a5014b23c26a2
Panos Karabelas <panosconroe@hotmail.com>
2023-08-04 10:32:06 +0100

    [Editor] Fixed some Texture Viewer sizing issues at higher DPIs

--- editor/Widgets/TextureViewer.cpp
+++ editor/Widgets/TextureViewer.cpp
@@ -43,7 +43,7 @@ namespace
     static bool m_channel_g               = true;
     static bool m_channel_b               = true;
     static bool m_channel_a               = true;
-    static bool m_gamma_correct           = false;
+    static bool m_gamma_correct           = true;
     static bool m_pack                    = false;
     static bool m_boost                   = false;
     static bool m_abs                     = false;
@@ -70,7 +70,7 @@ void TextureViewer::OnTick()
 
 void TextureViewer::OnTickVisible()
 {
-    // Get render targets
+    // get render targets
     static vector<string> render_target_options;
     if (render_target_options.empty())
     {
@@ -84,107 +84,115 @@ void TextureViewer::OnTickVisible()
         }
     }
 
-    // Display the selected texture
-    if (shared_ptr<RHI_Texture> texture = Renderer::GetRenderTarget(static_cast<Renderer_RenderTexture>(m_texture_index)))
-    {
-        // Calculate a percentage that once multiplied with the texture dimensions, the texture will always be displayed within the window.
-        float bottom_padding              = 200.0f; // to fit the information text
-        float texture_shrink_percentage_x = ImGui::GetWindowWidth() / static_cast<float>(texture->GetWidth()) * 0.95f; // 0.95 to avoid not be hidden by the scroll bar
-        float texture_shrink_percentage_y = ImGui::GetWindowHeight() / static_cast<float>(texture->GetHeight() + bottom_padding);
-        float texture_shrink_percentage   = min(texture_shrink_percentage_x, texture_shrink_percentage_y);
-
-        // Texture
-        float virtual_width = static_cast<float>(texture->GetWidth()) * texture_shrink_percentage;
-        float virtual_height = static_cast<float>(texture->GetHeight()) * texture_shrink_percentage;
-        ImGuiSp::image(texture.get(), Vector2(virtual_width, virtual_height), ImColor(255, 255, 255, 255), ImColor(0, 0, 0, 255));
-
-        // Magnifying glass
-        if (m_magnifying_glass && ImGui::IsItemHovered())
-        {
-            const float region_sz   = 32.0f;
-            const float zoom        = 16.0f;
-            const ImVec4 tint_col   = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); // No tint
-            const ImVec4 border_col = ImVec4(1.0f, 1.0f, 1.0f, 0.5f); // 50% opaque white
-
-            ImVec2 pos     = ImGui::GetCursorScreenPos();
-            ImGuiIO& io    = ImGui::GetIO();
-            float region_x = io.MousePos.x - pos.x - region_sz * 0.5f;
-            float region_y = io.MousePos.y - pos.y - region_sz * 0.5f;
-
-            ImGui::BeginTooltip();
-            {
-                region_x = clamp(region_x, 0.0f, virtual_width - region_sz);
-                region_y = clamp(region_y, 0.0f, virtual_height - region_sz);
-
-                ImVec2 uv0 = ImVec2(region_x / virtual_width, region_y / virtual_height);
-                ImVec2 uv1 = ImVec2((region_x + region_sz) / virtual_width, (region_y + region_sz) / virtual_height);
-                ImGui::Image(static_cast<ImTextureID>(texture.get()), ImVec2(region_sz * zoom, region_sz * zoom), uv0, uv1, tint_col, border_col);
-            }
-            ImGui::EndTooltip();
-        }
-
-        // Disable for now as it's buggy.
-        //ImGui::Checkbox("Magnifying glass", &m_magnifying_glass);
-
-        name          = texture->GetObjectName();
-        width         = texture->GetWidth();
-        height        = texture->GetHeight();
-        mip_count     = texture->GetMipCount();
-        channel_count = texture->GetChannelCount();
-        rhi_format    = texture->GetFormat();
-        texture_id    = texture->GetObjectId();
-    }
-
-    // Render target
-    ImGui::Text("Render target");
-    ImGui::SameLine();
-    ImGuiSp::combo_box("##render_target", render_target_options, &m_texture_index);
-
-    // Mip level
-    ImGui::SameLine();
-    ImGui::PushItemWidth(100);
-    ImGui::InputInt("Mip", &mip_level);
-    mip_level = Math::Helper::Clamp(mip_level, 0, static_cast<int>(mip_count) - 1);
-    ImGui::PopItemWidth();
-
-    // Properties
+    // texture
     ImGui::BeginGroup();
     {
-        // Information
-        ImGui::BeginGroup();
-        ImGui::Text("Name: %s",          name.c_str());
-        ImGui::Text("Dimensions: %dx%d", width, height);
-        ImGui::Text("Channels: %d",      channel_count);
-        ImGui::Text("Format: %s",        rhi_format_to_string(rhi_format));
-        ImGui::Text("Mips: %d",          mip_count);
-        ImGui::EndGroup();
-    
-        // Channels
+        if (shared_ptr<RHI_Texture> texture = Renderer::GetRenderTarget(static_cast<Renderer_RenderTexture>(m_texture_index)))
+        {
+            // calculate a percentage that once multiplied with the texture dimensions, the texture will always be displayed within the window.
+            float bottom_padding              = 200.0f * Spartan::Window::GetDpiScale(); // to fit the information text
+            float texture_shrink_percentage_x = ImGui::GetWindowWidth() / static_cast<float>(texture->GetWidth()) * 0.95f; // 0.95 to avoid not be hidden by the scroll bar
+            float texture_shrink_percentage_y = ImGui::GetWindowHeight() / static_cast<float>(texture->GetHeight() + bottom_padding);
+            float texture_shrink_percentage   = min(texture_shrink_percentage_x, texture_shrink_percentage_y);
+
+            // texture
+            float virtual_width  = static_cast<float>(texture->GetWidth()) * texture_shrink_percentage;
+            float virtual_height = static_cast<float>(texture->GetHeight()) * texture_shrink_percentage;
+            ImGuiSp::image(texture.get(), Vector2(virtual_width, virtual_height), ImColor(255, 255, 255, 255), ImColor(0, 0, 0, 255));
+
+            // magnifying glass
+            if (m_magnifying_glass && ImGui::IsItemHovered())
+            {
+                const float region_sz   = 32.0f;
+                const float zoom        = 16.0f;
+                const ImVec4 tint_col   = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); // No tint
+                const ImVec4 border_col = ImVec4(1.0f, 1.0f, 1.0f, 0.5f); // 50% opaque white
+
+                ImVec2 pos     = ImGui::GetCursorScreenPos();
+                ImGuiIO& io    = ImGui::GetIO();
+                float region_x = io.MousePos.x - pos.x - region_sz * 0.5f;
+                float region_y = io.MousePos.y - pos.y - region_sz * 0.5f;
+
+                ImGui::BeginTooltip();
+                {
+                    region_x = clamp(region_x, 0.0f, virtual_width - region_sz);
+                    region_y = clamp(region_y, 0.0f, virtual_height - region_sz);
+
+                    ImVec2 uv0 = ImVec2(region_x / virtual_width, region_y / virtual_height);
+                    ImVec2 uv1 = ImVec2((region_x + region_sz) / virtual_width, (region_y + region_sz) / virtual_height);
+                    ImGui::Image(static_cast<ImTextureID>(texture.get()), ImVec2(region_sz * zoom, region_sz * zoom), uv0, uv1, tint_col, border_col);
+                }
+                ImGui::EndTooltip();
+            }
+
+            // disable for now as it's buggy.
+            //ImGui::Checkbox("Magnifying glass", &m_magnifying_glass);
+
+            name          = texture->GetObjectName();
+            width         = texture->GetWidth();
+            height        = texture->GetHeight();
+            mip_count     = texture->GetMipCount();
+            channel_count = texture->GetChannelCount();
+            rhi_format    = texture->GetFormat();
+            texture_id    = texture->GetObjectId();
+        }
+    }
+    ImGui::EndGroup();
+
+    // properties
+    ImGui::BeginGroup();
+    {
+        // render target
+        ImGui::Text("Render target");
         ImGui::SameLine();
-        ImGui::BeginGroup();
-        ImGui::Text("Channels");
-        ImGui::Checkbox("R", &m_channel_r);
-        ImGui::Checkbox("G", &m_channel_g);
-        ImGui::Checkbox("B", &m_channel_b);
-        ImGui::Checkbox("A", &m_channel_a);
-        ImGui::EndGroup();
-    
-        // Misc
+        ImGuiSp::combo_box("##render_target", render_target_options, &m_texture_index);
+
+        // mip level
         ImGui::SameLine();
+        ImGui::PushItemWidth(85 * Spartan::Window::GetDpiScale());
+        ImGui::InputInt("Mip", &mip_level);
+        ImGui::PopItemWidth();
+        mip_level = Math::Helper::Clamp(mip_level, 0, static_cast<int>(mip_count) - 1);
+
         ImGui::BeginGroup();
-        ImGui::Checkbox("Gamma correct", &m_gamma_correct);
-        ImGui::Checkbox("Pack", &m_pack);
-        ImGui::Checkbox("Boost", &m_boost);
-        ImGui::EndGroup();
-        ImGui::SameLine();
-        ImGui::BeginGroup();
-        ImGui::Checkbox("Abs", &m_abs);
-        ImGui::Checkbox("Point sampling", &m_point_sampling);
+        {
+            // information
+            ImGui::BeginGroup();
+            ImGui::Text("Name: %s",          name.c_str());
+            ImGui::Text("Dimensions: %dx%d", width, height);
+            ImGui::Text("Channels: %d",      channel_count);
+            ImGui::Text("Format: %s",        rhi_format_to_string(rhi_format));
+            ImGui::Text("Mips: %d",          mip_count);
+            ImGui::EndGroup();
+        
+            // channels
+            ImGui::SameLine();
+            ImGui::BeginGroup();
+            ImGui::Text("Channels");
+            ImGui::Checkbox("R", &m_channel_r);
+            ImGui::Checkbox("G", &m_channel_g);
+            ImGui::Checkbox("B", &m_channel_b);
+            ImGui::Checkbox("A", &m_channel_a);
+            ImGui::EndGroup();
+        
+            // misc
+            ImGui::SameLine();
+            ImGui::BeginGroup();
+            ImGui::Checkbox("Gamma correct", &m_gamma_correct);
+            ImGui::Checkbox("Pack", &m_pack);
+            ImGui::Checkbox("Boost", &m_boost);
+            ImGui::EndGroup();
+            ImGui::SameLine();
+            ImGui::BeginGroup();
+            ImGui::Checkbox("Abs", &m_abs);
+            ImGui::Checkbox("Point sampling", &m_point_sampling);
+            ImGui::EndGroup();
+        }
         ImGui::EndGroup();
     }
     ImGui::EndGroup();
     
-    // Map changes
+    // map changes
     m_visualisation_flags |=  m_channel_r      ? Visualise_Channel_R    : 0;
     m_visualisation_flags |=  m_channel_g      ? Visualise_Channel_G    : 0;
     m_visualisation_flags |=  m_channel_b      ? Visualise_Channel_B    : 0;
@@ -194,7 +202,6 @@ void TextureViewer::OnTickVisible()
     m_visualisation_flags |=  m_boost          ? Visualise_Boost        : 0;
     m_visualisation_flags |=  m_abs            ? Visualise_Abs          : 0;
     m_visualisation_flags |=  m_point_sampling ? Visualise_Sample_Point : 0;
-
 }
 
 uint32_t TextureViewer::GetVisualisationFlags()
