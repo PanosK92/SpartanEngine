7b8790fe7198a2e047e552ad602afc0c749fe18e
Panos Karabelas <panosconroe@hotmail.com>
2023-08-24 10:42:53 +0100

    [ThirdParty] Updated FMOD to latest

--- third_party/fmod/fmod_common.h
+++ third_party/fmod/fmod_common.h
@@ -56,7 +56,7 @@ typedef struct FMOD_ASYNCREADINFO  FMOD_ASYNCREADINFO;
 /*
     FMOD constants
 */
-#define FMOD_VERSION    0x00020212                     /* 0xaaaabbcc -> aaaa = product version, bb = major version, cc = minor version.*/
+#define FMOD_VERSION    0x00020217                     /* 0xaaaabbcc -> aaaa = product version, bb = major version, cc = minor version.*/
 
 typedef unsigned int FMOD_DEBUG_FLAGS;
 #define FMOD_DEBUG_LEVEL_NONE                       0x00000000
