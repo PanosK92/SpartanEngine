24222323bd06f0b32bd1f9312de19568131241f6
Nickelium <trantr.be@gmail.com>
2023-09-25 23:28:42 +0200

    Integrate first steps for Bends screenspace shadows

--- runtime/Rendering/Renderer_Resources.cpp
+++ runtime/Rendering/Renderer_Resources.cpp
@@ -55,8 +55,8 @@ namespace Spartan
         array<shared_ptr<RHI_BlendState>, 3>        m_blend_states;
 
         // renderer resources
-        array<shared_ptr<RHI_Texture>, 28>       m_render_targets;
-        array<shared_ptr<RHI_Shader>, 45>        m_shaders;
+        array<shared_ptr<RHI_Texture>, 29>       m_render_targets;
+        array<shared_ptr<RHI_Shader>, Renderer::numberOfShaders>        m_shaders;
         array<shared_ptr<RHI_Sampler>, 7>        m_samplers;
         array<shared_ptr<RHI_ConstantBuffer>, 3> m_constant_buffers;
         shared_ptr<RHI_StructuredBuffer>         m_structured_buffer;
@@ -205,6 +205,9 @@ namespace Spartan
 
             // SSR - Mips are used to emulate roughness for surfaces which require it
             render_target(Renderer_RenderTexture::ssr) = make_shared<RHI_Texture2D>(width_render, height_render, mip_count, RHI_Format::R16G16B16A16_Float, RHI_Texture_Uav | RHI_Texture_Srv | RHI_Texture_PerMipViews, "rt_ssr");
+
+            // SSS
+            render_target(Renderer_RenderTexture::sss) = make_unique<RHI_Texture2D>(width_render, height_render, 1, RHI_Format::R11G11B10_Float, RHI_Texture_Uav | RHI_Texture_Srv | RHI_Texture_ClearOrBlit, "rt_sss");
 
             // SSGI
             render_target(Renderer_RenderTexture::ssgi)          = make_unique<RHI_Texture2D>(width_render, height_render, 1, RHI_Format::R16G16B16A16_Float, RHI_Texture_Uav | RHI_Texture_Srv, "rt_ssgi");
@@ -425,6 +428,14 @@ namespace Spartan
         shader(Renderer_Shader::ssr_c) = make_shared<RHI_Shader>();
         shader(Renderer_Shader::ssr_c)->Compile(RHI_Shader_Compute, shader_dir + "ssr.hlsl", async);
 
+        // SSS
+        shader(Renderer_Shader::sss_c) = make_shared<RHI_Shader>();
+        shader(Renderer_Shader::sss_c)->Compile(RHI_Shader_Compute, shader_dir + "sss.hlsl", async);
+
+        // Bend SSS
+        shader(Renderer_Shader::bend_sss_c) = make_shared<RHI_Shader>();
+        shader(Renderer_Shader::bend_sss_c)->Compile(RHI_Shader_Compute, shader_dir + "bend_sss.hlsl", async);
+
         // Temporal filter
         shader(Renderer_Shader::temporal_filter_c) = make_shared<RHI_Shader>();
         shader(Renderer_Shader::temporal_filter_c)->Compile(RHI_Shader_Compute, shader_dir + "temporal_filter.hlsl", async);
@@ -565,12 +576,12 @@ namespace Spartan
         m_structured_buffer = nullptr;
     }
 
-    array<shared_ptr<RHI_Texture>, 28>& Renderer::GetRenderTargets()
+    array<shared_ptr<RHI_Texture>, 29>& Renderer::GetRenderTargets()
     {
         return m_render_targets;
     }
 
-    array<shared_ptr<RHI_Shader>, 45>& Renderer::GetShaders()
+    array<shared_ptr<RHI_Shader>, Renderer::numberOfShaders>& Renderer::GetShaders()
     {
         return m_shaders;
     }
