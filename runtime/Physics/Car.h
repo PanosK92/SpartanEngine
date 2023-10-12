6496370440b0f66edc83daa3f182c374b56e83bd
Panos Karabelas <PanosConroe@hotmail.com>
2023-10-12 21:28:05 +0100

    [Car] Improved gearbox simulation

--- runtime/Physics/Car.h
+++ runtime/Physics/Car.h
@@ -55,10 +55,14 @@ namespace Spartan
         void ApplyForces();
         void UpdateTransforms();
 
-        float m_torque_newtons                        = 0.0f;
-        float m_steering_angle_radians                = 0.0f;
+        float m_engine_torque                         = 0.0f;
+        float m_engine_rpm                            = 0.0f;
+        float m_sterring_angle                        = 0.0f;
         float m_break_force                           = 0.0f;
         bool m_wants_to_reverse                       = false;
+        uint32_t m_current_gear                       = 1;
+        float m_last_shift_time                       = 0.0f; 
+        bool m_is_shifting                            = false;
         btRaycastVehicle* m_vehicle                   = nullptr;
         Transform* m_vehicle_steering_wheel_transform = nullptr;
         btRigidBody* m_vehicle_chassis                = nullptr;
