4fdf3a25a1b2af0262325724d7bd5b8763b1142e
Panos Karabelas <PanosConroe@hotmail.com>
2023-10-21 20:17:32 +0100

    [Renderer] Fixed an issue where transparents would not write to the depth buffer

--- runtime/Rendering/Renderer.h
+++ runtime/Rendering/Renderer.h
@@ -160,10 +160,10 @@ namespace Spartan
 
         // passes - core
         static void Pass_Frame(RHI_CommandList* cmd_list);
-        static void Pass_ShadowMaps(RHI_CommandList* cmd_list, const bool is_transparent_pass);
+        static void Pass_ShadowMaps(RHI_CommandList* cmd_list, const bool is_transparent_pass = false);
         static void Pass_ReflectionProbes(RHI_CommandList* cmd_list);
-        static void Pass_Depth_Prepass(RHI_CommandList* cmd_list);
-        static void Pass_GBuffer(RHI_CommandList* cmd_list, const bool is_transparent_pass);
+        static void Pass_Depth_Prepass(RHI_CommandList* cmd_list, const bool is_transparent_pass = false);
+        static void Pass_GBuffer(RHI_CommandList* cmd_list, const bool is_transparent_pass = false);
         static void Pass_Ssgi(RHI_CommandList* cmd_list);
         static void Pass_Ssr(RHI_CommandList* cmd_list, RHI_Texture* tex_in);
         static void Pass_Sss_Bend(RHI_CommandList* cmd_list);
@@ -187,9 +187,9 @@ namespace Spartan
         static void Pass_Debanding(RHI_CommandList* cmd_list, RHI_Texture* tex_in, RHI_Texture* tex_out);
         static void Pass_Bloom(RHI_CommandList* cmd_list, RHI_Texture* tex_in, RHI_Texture* tex_out);
         // passes - Lighting
-        static void Pass_Light(RHI_CommandList* cmd_list, const bool is_transparent_pass);
-        static void Pass_Light_Composition(RHI_CommandList* cmd_list, RHI_Texture* tex_out, const bool is_transparent_pass);
-        static void Pass_Light_ImageBased(RHI_CommandList* cmd_list, RHI_Texture* tex_out, const bool is_transparent_pass);
+        static void Pass_Light(RHI_CommandList* cmd_list, const bool is_transparent_pass = false);
+        static void Pass_Light_Composition(RHI_CommandList* cmd_list, RHI_Texture* tex_out, const bool is_transparent_pass = false);
+        static void Pass_Light_ImageBased(RHI_CommandList* cmd_list, RHI_Texture* tex_out, const bool is_transparent_pass = false);
         // passes - amd fidelityfx
         static void Pass_Ffx_Cas(RHI_CommandList* cmd_list, RHI_Texture* tex_in, RHI_Texture* tex_out);
         static void Pass_Ffx_Spd(RHI_CommandList* cmd_list, RHI_Texture* tex);
