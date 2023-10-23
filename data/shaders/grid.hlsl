d0ae9531aa7cb603180271b4c5265b109599ed5a
Panos Karabelas <PanosConroe@hotmail.com>
2023-10-23 20:34:50 +0100

    [Renderer] Grid rendering now fades out and has an anti-moire algorithm

--- data/shaders/grid.hlsl
+++ data/shaders/grid.hlsl
@@ -23,25 +23,50 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "common.hlsl"
 //====================
 
-static const float4 grid_color = float4(0.5f, 0.5f, 0.5f, 1.0f);
+// properties
+static const float4 grid_color          = float4(0.5f, 0.5f, 0.5f, 1.0f);
+static const float  fade_start          = 0.3f;
+static const float  fade_end            = 0.6f;
+static const float  anti_moire_start    = 0.0f;
+static const float  anti_moire_strength = 100.0f;
+
+// increase line thickness the further away we get, this counteracts the moire pattern
+float compute_anti_moire_factor(float2 uv, float moire_start, float moire_end)
+{
+    float2 centered_uv         = uv - 0.5f;
+    float distance_from_center = length(centered_uv);
+    return lerp(1.0f, anti_moire_strength, saturate((distance_from_center - moire_start) / (moire_end - moire_start)));
+}
+
+// fade out the further away we get from the center, this prevents us from seeing the edge of the grid
+float compute_fade_factor(float2 uv, float fade_start, float fade_end)
+{
+    float2 centered_uv         = uv - 0.5f;
+    float distance_from_center = length(centered_uv);
+    return saturate((fade_end - distance_from_center) / (fade_end - fade_start));
+}
 
 float4 mainPS(Pixel_PosUv input) : SV_TARGET
 {
     const float2 properties    = pass_get_f3_value().xy;
     const float line_interval  = properties.x;
-    const float line_thickness = properties.y;
+    float line_thickness = properties.y;
+
+    // anti-moire
+    line_thickness *= compute_anti_moire_factor(input.uv, anti_moire_start, fade_end);
     
     // calculate the modulated distance for both x and y coordinates
     float mod_x = fmod(input.uv.x, line_interval);
     float mod_y = fmod(input.uv.y, line_interval);
     
     // use step function to determine if the pixel is near a line
-    float line_x = step(line_thickness, mod_x) - step(line_interval - line_thickness, mod_x);
-    float line_y = step(line_thickness, mod_y) - step(line_interval - line_thickness, mod_y);
+    float line_x            = step(line_thickness, mod_x) - step(line_interval - line_thickness, mod_x);
+    float line_y            = step(line_thickness, mod_y) - step(line_interval - line_thickness, mod_y);
     
     // combine line_x and line_y to decide if either is true, then color it as a line
     float is_line = max(1.0f - line_x, 1.0f - line_y);
 
     // either grid_color or 0 based on is_line
-    return is_line * grid_color + (1.0f - is_line) * 0.0f;
+    float fade_factor = compute_fade_factor(input.uv, fade_start, fade_end);
+    return fade_factor * (is_line * grid_color + (1.0f - is_line) * 0.0f);
 }
