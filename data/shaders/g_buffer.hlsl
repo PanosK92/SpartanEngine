ad12c192d533574b43c45e5b2ef2d531ba07ef55
Panos Karabelas <panosconroe@hotmail.com>
2023-11-02 12:01:30 +0000

    [Rendering] Optimized g-buffer pass so that it doesn't do expensive operations (texture sampling, normal mapping etc) beyond 180 world units, at which point, you can't tell anyway

--- data/shaders/g_buffer.hlsl
+++ data/shaders/g_buffer.hlsl
@@ -20,13 +20,15 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "common.hlsl"
 //====================
 
+static const float g_quality_distance_low = 180.0f;
+
 struct PixelInputType
 {
     float4 position             : SV_POSITION;
     float2 uv                   : TEXCOORD;
     float3 normal_world         : WORLD_NORMAL;
     float3 tangent_world        : WORLD_TANGENT;
-    float4 position_world       : WORLD_POS;
+    float3 position_world       : WORLD_POS;
     float4 position_ss_current  : SCREEN_POS;
     float4 position_ss_previous : SCREEN_POS_PREVIOUS;
 };
@@ -48,7 +50,7 @@ PixelInputType mainVS(Vertex_PosUvNorTan input, uint instance_id : SV_InstanceID
     // position
     output.position             = compute_screen_space_position(input, instance_id, buffer_pass.transform, buffer_frame.view_projection);
     output.position_ss_current  = output.position;
-    output.position_ss_previous = compute_screen_space_position(input, instance_id, pass_get_transform_previous(), buffer_frame.view_projection_previous);
+    output.position_ss_previous = compute_screen_space_position(input, instance_id, pass_get_transform_previous(), buffer_frame.view_projection_previous, output.position_world);
     // normals
     output.normal_world  = normalize(mul(input.normal,  (float3x3)buffer_pass.transform)).xyz;
     output.tangent_world = normalize(mul(input.tangent, (float3x3)buffer_pass.transform)).xyz;
@@ -97,55 +99,39 @@ float3 smaple_normal(float2 uv, float slope)
 
 PixelOutputType mainPS(PixelInputType input)
 {
+    // initial g-buffer values
+    float4 albedo      = buffer_material.color;
+    float3 normal      = input.normal_world.xyz;
+    float roughness    = buffer_material.roughness;
+    float metalness    = buffer_material.metallness;
+    float occlusion    = 1.0f;
+    float emission     = 0.0f;
+    float2 velocity_uv = 0.0f;
+    
     // velocity
     float2 position_uv_current  = ndc_to_uv((input.position_ss_current.xy / input.position_ss_current.w) - buffer_frame.taa_jitter_current);
     float2 position_uv_previous = ndc_to_uv((input.position_ss_previous.xy / input.position_ss_previous.w) - buffer_frame.taa_jitter_previous);
-    float2 velocity_uv          = position_uv_current - position_uv_previous;
+    velocity_uv                 = position_uv_current - position_uv_previous;
 
     // uv
     float2 uv  = input.uv;
     uv         = float2(uv.x * buffer_material.tiling.x + buffer_material.offset.x, uv.y * buffer_material.tiling.y + buffer_material.offset.y);
     uv        += float(buffer_frame.time * 0.1f) * material_texture_animate();
     
-    // parallax mapping
-    if (has_texture_height())
-    {
-        float scale = buffer_material.height * 0.01f;
-
-        float3x3 world_to_tangent      = make_world_to_tangent_matrix(input.normal_world, input.tangent_world);
-        float3 camera_to_pixel_world   = normalize(buffer_frame.camera_position - input.position_world.xyz);
-        float3 camera_to_pixel_tangent = normalize(mul(camera_to_pixel_world, world_to_tangent));
-        float height                   = tex_material_height.Sample(samplers[sampler_anisotropic_wrap], uv).r - 0.5f;
-        uv                             += (camera_to_pixel_tangent.xy / camera_to_pixel_tangent.z) * height * scale;
-    }
-
     // alpha mask
     float alpha_mask = 1.0f;
     if (has_texture_alpha_mask())
     {
         alpha_mask = tex_material_mask.Sample(samplers[sampler_anisotropic_wrap], uv).r;
     }
-
-    // normal
-    float3 normal = input.normal_world.xyz;
-    float slope   = compute_slope(normal);
-    if (has_texture_normal())
-    {
-        // get tangent space normal and apply the user defined intensity, then transform it to world space
-        float3 tangent_normal      = normalize(unpack(smaple_normal(uv, slope)));
-        float normal_intensity     = clamp(buffer_material.normal, 0.012f, buffer_material.normal);
-        tangent_normal.xy         *= saturate(normal_intensity);
-        float3x3 tangent_to_world  = make_tangent_to_world_matrix(input.normal_world, input.tangent_world);
-        normal                     = normalize(mul(tangent_normal, tangent_to_world).xyz);
-    }
     
     // albedo
-    float4 albedo = buffer_material.color;
+    float slope = compute_slope(normal);
     if (has_texture_albedo())
     {
         float4 albedo_sample = sample_albedo(uv, slope);
 
-        // read albedo's alpha channel as an alpha mask as well.
+        // read albedo's alpha channel as an alpha mask as well
         alpha_mask      = min(alpha_mask, albedo_sample.a);
         albedo_sample.a = 1.0f;
         
@@ -157,66 +143,92 @@ PixelOutputType mainPS(PixelInputType input)
     if (alpha_mask <= ALPHA_THRESHOLD)
         discard;
 
-    // roughness + metalness
-    float roughness = buffer_material.roughness;
-    float metalness = buffer_material.metallness;
-    {
-        if (!has_single_texture_roughness_metalness())
-        {
-            if (has_texture_roughness())
-            {
-                roughness *= tex_material_roughness.Sample(samplers[sampler_anisotropic_wrap], uv).r;
-            }
-
-            if (has_texture_metalness())
-            {
-                metalness *= tex_material_metallness.Sample(samplers[sampler_anisotropic_wrap], uv).r;
-            }
-        }
-        else
-        {
-            if (has_texture_roughness())
-            {
-                roughness *= tex_material_roughness.Sample(samplers[sampler_anisotropic_wrap], uv).g;
-            }
-
-            if (has_texture_metalness())
-            {
-                metalness *= tex_material_metallness.Sample(samplers[sampler_anisotropic_wrap], uv).b;
-            }
-        }
-    }
+    // determine shading quality
+    float3 camera_to_pixel_world = buffer_frame.camera_position - input.position_world.xyz;
+    float pixel_distance         = length(camera_to_pixel_world);
+    bool high_quality            = pixel_distance < g_quality_distance_low;
     
-    // occlusion
-    float occlusion = 1.0f;
-    if (has_texture_occlusion())
+    if (high_quality)
     {
-        occlusion = tex_material_occlusion.Sample(samplers[sampler_anisotropic_wrap], uv).r;
+        // parallax mapping
+        if (has_texture_height())
+        {
+            float scale = buffer_material.height * 0.01f;
+
+            float3x3 world_to_tangent       = make_world_to_tangent_matrix(input.normal_world, input.tangent_world);
+            float3 camera_to_pixel_tangent  = normalize(mul(normalize(camera_to_pixel_world), world_to_tangent));
+            float height                    = tex_material_height.Sample(samplers[sampler_anisotropic_wrap], uv).r - 0.5f;
+            uv                             += (camera_to_pixel_tangent.xy / camera_to_pixel_tangent.z) * height * scale;
+        }
+        
+        // normal mapping
+        if (has_texture_normal())
+        {
+            // get tangent space normal and apply the user defined intensity, then transform it to world space
+            float3 tangent_normal      = normalize(unpack(smaple_normal(uv, slope)));
+            float normal_intensity     = clamp(buffer_material.normal, 0.012f, buffer_material.normal);
+            tangent_normal.xy         *= saturate(normal_intensity);
+            float3x3 tangent_to_world  = make_tangent_to_world_matrix(input.normal_world, input.tangent_world);
+            normal                     = normalize(mul(tangent_normal, tangent_to_world).xyz);
+        }
+        
+        // roughness + metalness
+        {
+            if (!has_single_texture_roughness_metalness())
+            {
+                if (has_texture_roughness())
+                {
+                    roughness *= tex_material_roughness.Sample(samplers[sampler_anisotropic_wrap], uv).r;
+                }
+
+                if (has_texture_metalness())
+                {
+                    metalness *= tex_material_metallness.Sample(samplers[sampler_anisotropic_wrap], uv).r;
+                }
+            }
+            else
+            {
+                if (has_texture_roughness())
+                {
+                    roughness *= tex_material_roughness.Sample(samplers[sampler_anisotropic_wrap], uv).g;
+                }
+
+                if (has_texture_metalness())
+                {
+                    metalness *= tex_material_metallness.Sample(samplers[sampler_anisotropic_wrap], uv).b;
+                }
+            }
+        }
+        
+        // occlusion
+        if (has_texture_occlusion())
+        {
+            occlusion = tex_material_occlusion.Sample(samplers[sampler_anisotropic_wrap], uv).r;
+        }
+
+        // emission
+        if (has_texture_emissive())
+        {
+            float3 emissive_color  = tex_material_emission.Sample(samplers[sampler_anisotropic_wrap], uv).rgb;
+            emission               = luminance(emissive_color);
+            albedo.rgb            += emissive_color;
+        }
+
+        // specular anti-aliasing
+        {
+            static const float strength           = 1.0f;
+            static const float max_roughness_gain = 0.02f;
+
+            float roughness2         = roughness * roughness;
+            float3 dndu              = ddx(normal), dndv = ddy(normal);
+            float variance           = (dot(dndu, dndu) + dot(dndv, dndv));
+            float kernelRoughness2   = min(variance * strength, max_roughness_gain);
+            float filteredRoughness2 = saturate(roughness2 + kernelRoughness2);
+            roughness                = fast_sqrt(filteredRoughness2);
+        }
     }
 
-    // Emission
-    float emission = 0.0f;
-    if (has_texture_emissive())
-    {
-        float3 emissive_color  = tex_material_emission.Sample(samplers[sampler_anisotropic_wrap], uv).rgb;
-        emission               = luminance(emissive_color);
-        albedo.rgb            += emissive_color;
-    }
-
-    // Specular anti-aliasing
-    {
-        static const float strength           = 1.0f;
-        static const float max_roughness_gain = 0.02f;
-
-        float roughness2         = roughness * roughness;
-        float3 dndu              = ddx(normal), dndv = ddy(normal);
-        float variance           = (dot(dndu, dndu) + dot(dndv, dndv));
-        float kernelRoughness2   = min(variance * strength, max_roughness_gain);
-        float filteredRoughness2 = saturate(roughness2 + kernelRoughness2);
-        roughness                = fast_sqrt(filteredRoughness2);
-    }
-
-    // Write to G-Buffer
+    // write to g-buffer
     PixelOutputType g_buffer;
     g_buffer.albedo                 = albedo;
     g_buffer.normal                 = float4(normal, buffer_material.sheen);
