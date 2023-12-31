61b94cef8d75e91188a3579530782d09d19cfc25
Panos Karabelas <panosconroe@hotmail.com>
2023-12-31 12:39:19 +0000

    [License] Updated copyright date to 2024

--- data/shaders/g_buffer.hlsl
+++ data/shaders/g_buffer.hlsl
@@ -1,5 +1,6 @@
 /*
-Copyright(c) 2016-2023 Panos Karabelas
+Copyright(c) 2016-2024 Panos Karabelas
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
