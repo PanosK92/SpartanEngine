c58c07adc74f35909e43db7d3edd6eb014d6fe5c
Panos Karabelas <panosconroe@hotmail.com>
2023-12-31 17:45:37 +0000

    [D3D12] Build fix

--- runtime/RHI/D3D12/D3D12_CommandList.cpp
+++ runtime/RHI/D3D12/D3D12_CommandList.cpp
@@ -187,7 +187,6 @@ namespace Spartan
         OnPreDrawDispatch();
 
         static_cast<ID3D12GraphicsCommandList*>(m_rhi_resource)->Dispatch(x, y, z);
-        Profiler::m_rhi_dispatch++;
     }
 
     void RHI_CommandList::Blit(RHI_Texture* source, RHI_Texture* destination, const bool blit_mips)
