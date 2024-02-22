28d6877f2eabbf8b18e1d41c4252fdab4390b683
N. Pattakos <pattakosn@fastmail.com>
2024-02-23 00:20:49 +0100

    add missing header for MAX/MINFLOAT values

--- runtime/Math/Rectangle.h
+++ runtime/Math/Rectangle.h
@@ -23,6 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 //= INCLUDES =======
 #include <memory>
+#include <cfloat>
 #include "Vector2.h"
 //==================
 
