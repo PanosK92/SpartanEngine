69da843f6d71ebfaa1eec0357949ad7e32fc9a89
Panos Karabelas <panosconroe@hotmail.com>
2024-04-04 13:14:35 +0100

    [Workflow] Updated automatic release action to latest

--- build_scripts/ci_test.bat
+++ build_scripts/ci_test.bat
@@ -1,6 +1,5 @@
-@echo off
-:: Run the Spartan engine executable for the specified API (Vulkan or D3D12) with the -ci_test flag
+:: run the Spartan engine executable for the specified API (Vulkan or D3D12) with the -ci_test flag
 binaries\spartan_%1.exe -ci_test
 
-:: Wait for 15 seconds to ensure the engine has finished
+:: wait for 15 seconds to ensure the engine has produced a frame
 timeout /t 15
