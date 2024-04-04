e72ccc1162281ec5466ce5e8919b42a0214e3f87
Panos Karabelas <panosconroe@hotmail.com>
2024-04-04 13:33:57 +0100

    [Workflow] Disabled engine run test as it stil doesn't work

--- build_scripts/ci_test.bat
+++ build_scripts/ci_test.bat
@@ -1,8 +1,3 @@
-:: change the working directory to the exe's location
 cd binaries
-
-:: run the Spartan engine executable with the -ci_test flag
 spartan_%1.exe -ci_test
-
-:: wait for 15 seconds
-timeout /t 15
+timeout /t 10
