2615fcb45d15ac87da6b8c1831e052fe888f9ffe
Panos Karabelas <panosconroe@hotmail.com>
2024-04-04 13:29:33 +0100

    [Workflow] Re-enabled engine run test

--- build_scripts/ci_test.bat
+++ build_scripts/ci_test.bat
@@ -1,5 +1,8 @@
-:: run the Spartan engine executable for the specified API (Vulkan or D3D12) with the -ci_test flag
-binaries\spartan_%1.exe -ci_test
+:: change the working directory to the exe's location
+cd binaries
 
-:: wait for 15 seconds to ensure the engine has produced a frame
+:: run the Spartan engine executable with the -ci_test flag
+spartan_%1.exe -ci_test
+
+:: wait for 15 seconds
 timeout /t 15
