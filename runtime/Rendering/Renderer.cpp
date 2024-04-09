a7bf62cde19adff5b00337c526fdd93ab7973ea6
Panos Karabelas <panosconroe@hotmail.com>
2024-04-09 14:09:10 +0100

    shaders: simplified material index and is_transparent cpu to gpu approach

--- runtime/Rendering/Renderer.cpp
+++ runtime/Rendering/Renderer.cpp
@@ -435,7 +435,7 @@ namespace Spartan
         m_cb_frame_cpu.taa_jitter_previous         = m_cb_frame_cpu.taa_jitter_current;
         m_cb_frame_cpu.taa_jitter_current          = jitter_offset;
         m_cb_frame_cpu.time                        = static_cast<float>(Timer::GetTimeSec());
-        m_cb_frame_cpu.delta_time                  = static_cast<float>(Timer::GetDeltaTimeSmoothedSec()); // removes stutters from motion related code
+        m_cb_frame_cpu.delta_time                  = static_cast<float>(Timer::GetDeltaTimeSec());
         m_cb_frame_cpu.frame                       = static_cast<uint32_t>(frame_num);
         m_cb_frame_cpu.resolution_scale            = GetOption<float>(Renderer_Option::ResolutionScale);
         m_cb_frame_cpu.hdr_enabled                 = GetOption<bool>(Renderer_Option::Hdr) ? 1.0f : 0.0f;
