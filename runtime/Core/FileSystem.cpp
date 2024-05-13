cb74b9d83d15553d4336281a1124a9114a1a0849
N. Pattakos <pattakosn@fastmail.com>
2024-05-14 01:33:16 +0200

    fix sdl includes: use the recommended way (ie file without SDL2/ or sdl/ prefix )

--- runtime/Core/FileSystem.cpp
+++ runtime/Core/FileSystem.cpp
@@ -21,7 +21,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 //= INCLUDES ============
 #include "pch.h"
-#include <SDL/SDL_misc.h>
+#include <SDL_misc.h>
 //=======================
 
 //= NAMESPACES =====
