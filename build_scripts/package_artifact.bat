544a2143254ef5b21dc59da5ce8845ca4b5209b7
Panos Karabelas <panosconroe@hotmail.com>
2024-05-16 17:57:25 +0100

    scrits: create a binary only 7z file

--- build_scripts/package_artifact.bat
+++ build_scripts/package_artifact.bat
@@ -1,1 +1,7 @@
+@echo off
+
+:: package everything for the full release
 build_scripts\7z.exe a build_%1.7z .\binaries\dxcompiler.dll .\binaries\fmod.dll .\binaries\data .\binaries\project .\binaries\spartan_%1.exe
+
+:: package binaries only for a binaries-only release
+build_scripts\7z.exe a binaries_only_%1.7z .\binaries\dxcompiler.dll .\binaries\fmod.dll .\binaries\data .\binaries\spartan_%1.exe
