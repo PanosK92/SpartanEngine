8c483392e5451ca83d9c716814936b6ef8f668c9
Panos Karabelas <panosconroe@hotmail.com>
2024-07-08 19:04:43 +0100

    [contributors] updated Sandro's contribution to also mention that he spotted lighting issues

--- editor/Widgets/TitleBar.cpp
+++ editor/Widgets/TitleBar.cpp
@@ -57,18 +57,18 @@ namespace
     { 
         vector<string> contributors =
         {
-            // role,  name,                country,       button text,   button url,                                               contribution,                      steam key
-            "Spartan, Iker Galardi,        Basque Country,   LinkedIn,   https://www.linkedin.com/in/iker-galardi/,                Linux port (WIP),                  N/A",
-            "Spartan, Jesse Guerrero,      United States,    LinkedIn,   https://www.linkedin.com/in/jguer,                        UX improvements,                   N/A",
-            "Spartan, Konstantinos Benos,  Greece,           Twitter,    https://twitter.com/deg3x,                                Editor theme & bug fixes,          N/A",
-            "Spartan, Nick Polyderopoulos, Greece,           LinkedIn,   https://www.linkedin.com/in/nick-polyderopoulos-21742397, UX improvements,                   N/A",
-            "Spartan, Panos Kolyvakis,     Greece,           LinkedIn,   https://www.linkedin.com/in/panos-kolyvakis-66863421a/,   Improved water buoyancy,           N/A",
-            "Spartan, Tri Tran,            Belgium,          LinkedIn,   https://www.linkedin.com/in/mtrantr/,                     Days Gone screen space shadows,    Starfield",
+            // role,  name,                country,       button text,   button url,                                               contribution,                             steam key
+            "Spartan, Iker Galardi,        Basque Country,   LinkedIn,   https://www.linkedin.com/in/iker-galardi/,                Linux port (WIP),                         N/A",
+            "Spartan, Jesse Guerrero,      United States,    LinkedIn,   https://www.linkedin.com/in/jguer,                        UX improvements,                          N/A",
+            "Spartan, Konstantinos Benos,  Greece,           Twitter,    https://twitter.com/deg3x,                                Editor theme & bug fixes,                 N/A",
+            "Spartan, Nick Polyderopoulos, Greece,           LinkedIn,   https://www.linkedin.com/in/nick-polyderopoulos-21742397, UX improvements,                          N/A",
+            "Spartan, Panos Kolyvakis,     Greece,           LinkedIn,   https://www.linkedin.com/in/panos-kolyvakis-66863421a/,   Improved water buoyancy,                  N/A",
+            "Spartan, Tri Tran,            Belgium,          LinkedIn,   https://www.linkedin.com/in/mtrantr/,                     Days Gone screen space shadows,           Starfield",
                                                              
-            "Hoplite, Apostolos Bouzalas,  Greece,           LinkedIn,   https://www.linkedin.com/in/apostolos-bouzalas,           Provided performance reports,      N/A",
-            "Hoplite, Marlon Wolfersdorf,  Northern Germany, Google,     https://www.google.com/search?q=Marlon+Wolfersdorf,       Nautilus ACES,                     N/A",
-            "Hoplite, Nikolas Pattakos,    Greece,           LinkedIn,   https://www.linkedin.com/in/nikolaspattakos/,             GCC compile fixes,                 N/A",
-            "Hoplite, Sandro Mtchedlidze,  Georgia,          Artstation, https://www.artstation.com/sandromch,                     Identified performance bottleneck, N/A",
+            "Hoplite, Apostolos Bouzalas,  Greece,           LinkedIn,   https://www.linkedin.com/in/apostolos-bouzalas,           Provided performance reports,             N/A",
+            "Hoplite, Marlon Wolfersdorf,  Northern Germany, Google,     https://www.google.com/search?q=Marlon+Wolfersdorf,       Nautilus ACES,                            N/A",
+            "Hoplite, Nikolas Pattakos,    Greece,           LinkedIn,   https://www.linkedin.com/in/nikolaspattakos/,             GCC compile fixes,                        N/A",
+            "Hoplite, Sandro Mtchedlidze,  Georgia,          Artstation, https://www.artstation.com/sandromch,                     Identified lighting & performance issues, N/A",
         };
 
         vector<string> comma_seperate_contributors(const vector<string>& contributors)
