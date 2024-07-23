864aef31b2b1092f0b4f700e57ac1fb9a707ee67
Panos Karabelas <panosconroe@hotmail.com>
2024-07-23 13:08:00 +0100

    [d3d12] build fix

--- runtime/Rendering/Renderer_Passes.cpp
+++ runtime/Rendering/Renderer_Passes.cpp
@@ -409,9 +409,9 @@ namespace Spartan
         cmd_list->SetConstantBuffer(Renderer_BindingsCb::frame, GetConstantBufferFrame());
 
         // structure buffers
-        cmd_list->SetStructuredBuffer(Renderer_BindingsUav::sb_materials, GetBuffer(Renderer_Buffer::Materials));
-        cmd_list->SetStructuredBuffer(Renderer_BindingsUav::sb_lights,    GetBuffer(Renderer_Buffer::Lights));
-        cmd_list->SetStructuredBuffer(Renderer_BindingsUav::sb_spd,       GetBuffer(Renderer_Buffer::Spd));
+        cmd_list->SetBuffer(Renderer_BindingsUav::sb_materials, GetBuffer(Renderer_Buffer::Materials));
+        cmd_list->SetBuffer(Renderer_BindingsUav::sb_lights,    GetBuffer(Renderer_Buffer::Lights));
+        cmd_list->SetBuffer(Renderer_BindingsUav::sb_spd,       GetBuffer(Renderer_Buffer::Spd));
     }
 
     void Renderer::ProduceFrame(RHI_CommandList* cmd_list_graphics, RHI_CommandList* cmd_list_compute)
