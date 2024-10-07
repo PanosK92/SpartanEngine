112b7951ae0ad30649c979ef5d8c2f086a70fea5
Panos Karabelas <panosconroe@hotmail.com>
2024-10-07 19:39:16 +0100

    [breadcrumbs] started the fidelityfx implementation

--- build_scripts/premake.lua
+++ build_scripts/premake.lua
@@ -69,7 +69,8 @@ API_LIBRARIES = {
 			"ffx_denoiser_x64",
 			"ffx_sssr_x64",
 			"ffx_brixelizer_x64",
-			"ffx_brixelizergi_x64"
+			"ffx_brixelizergi_x64",
+			"ffx_breadcrumbs_x64"
         },
         debug = {
             "spirv-cross-c_debug",
@@ -85,7 +86,8 @@ API_LIBRARIES = {
 			"ffx_denoiser_x64d",
 			"ffx_sssr_x64d",
 			"ffx_brixelizer_x64d",
-			"ffx_brixelizergi_x64d"
+			"ffx_brixelizergi_x64d",
+			"ffx_breadcrumbs_x64d"
         }
     },
     vulkan_linux = {
