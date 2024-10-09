a1920325a72bf330367c0e57bc46f3b806bc05be
Panos Karabelas <panosconroe@hotmail.com>
2024-10-09 23:45:10 +0100

    [vulkan] removed validation layer filter for timeline semaphore false positives since the latest sdk fixes it

--- runtime/RHI/Vulkan/Vulkan_Device.cpp
+++ runtime/RHI/Vulkan/Vulkan_Device.cpp
@@ -412,25 +412,14 @@ namespace Spartan
             {
                 // ignore some messages
                 {
-                    // false positives and minor
+                    // occlusion queries
+                    if (p_callback_data->messageIdNumber == 0xd39be754)
                     {
-
-                        // silence false positive synchronization error due to validation layer issue
-                        // check fix progress here: https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/7600
-                        if (p_callback_data->messageIdNumber == 0x29910a35)
-                            return VK_FALSE;
-
-                        // occlusion queries
-                        {
-                            if (p_callback_data->messageIdNumber == 0xd39be754)
-                            {
-                                // Validation Warning:
-                                // [BestPractices - QueryPool - Unavailable] Object 0 :
-                                // handle = 0x980b0000000002e, name = query_pool_occlusion, type = VK_OBJECT_TYPE_QUERY_POOL; | MessageID = 0xd39be754 | vkGetQueryPoolResults() :
-                                // QueryPool VkQueryPool 0x980b0000000002e[query_pool_occlusion] and query 0 : vkCmdBeginQuery() was never called.
-                                return VK_FALSE;
-                            }
-                        }
+                        // Validation Warning:
+                        // [BestPractices - QueryPool - Unavailable] Object 0 :
+                        // handle = 0x980b0000000002e, name = query_pool_occlusion, type = VK_OBJECT_TYPE_QUERY_POOL; | MessageID = 0xd39be754 | vkGetQueryPoolResults() :
+                        // QueryPool VkQueryPool 0x980b0000000002e[query_pool_occlusion] and query 0 : vkCmdBeginQuery() was never called.
+                        return VK_FALSE;
                     }
 
                     // legit but they spam every frame
