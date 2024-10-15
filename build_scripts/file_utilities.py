c5347fed0215ed41c1d52ead7b683810267cbef4
Panos Karabelas <panosconroe@hotmail.com>
2024-10-15 11:05:58 +0100

    [build_scripts] extraced file download and extraction functionality out of generate_project_files.py and into file_utilities.py, this way it the binaries themselves can download the assets

---
+++ build_scripts/file_utilities.py
@@ -0,0 +1,67 @@
+#Copyright(c) 2016-2024 Panos Karabelas
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+#copies of the Software, and to permit persons to whom the Software is furnished
+#to do so, subject to the following conditions :
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+#FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+#COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+#IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+from pathlib import Path
+import hashlib
+import requests
+from tqdm import tqdm
+
+def calculate_file_hash(file_path, hash_type="sha256"):
+    hash_func = hashlib.new(hash_type)
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_func.update(chunk)
+    return hash_func.hexdigest()
+
+def download_file(url, destination, expected_hash):
+    if os.path.exists(destination):
+        if calculate_file_hash(destination) == expected_hash:
+            print(f"File {destination} already exists with the correct hash. Skipping download.")
+            return
+
+    print(f"\nDownloading {destination}...")
+    os.makedirs(os.path.dirname(destination), exist_ok=True)
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+    
+    with open(destination, 'wb') as f:
+        for chunk in response.iter_content(block_size):
+            t.update(len(chunk))
+            f.write(chunk)
+    t.close()
+    
+    if total_size != 0 and t.n != total_size:
+        print("ERROR, something went wrong during download")
+        return
+    
+    if calculate_file_hash(destination) != expected_hash:
+        print(f"ERROR, hash mismatch for {destination}")
+        return
+
+def extract_archive(archive_path, destination_path, is_windows):
+    cmd = (
+        f"build_scripts\\7z.exe x {archive_path} -o{destination_path} -aoa"
+        if is_windows
+        else f"7za x {archive_path} -o{destination_path} -aoa"
+    )
+    print(f"Extracting {archive_path} to {destination_path}...")
+    os.system(cmd)
