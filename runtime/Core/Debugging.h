a88c2b91ce0f9cd34ff82b0538ed262e185006b5
Panos Karabelas <panosconroe@hotmail.com>
2024-10-28 15:19:34 +0000

    [vulkan] improved resource deletion logic to ensure that no resource is deleted while a command list is using it

--- runtime/Core/Debugging.h
+++ runtime/Core/Debugging.h
@@ -46,11 +46,11 @@ namespace Spartan
     private:
         inline static bool m_validation_layer_enabled        = false; // enables vulkan validation, high cpu overhead per draw
         inline static bool m_gpu_assisted_validation_enabled = false; // gpu-based validation, significant cpu and gpu cost
-        inline static bool m_breadcrumbs_enabled             = false; // breadcrumbs: tracks gpu crashes and writes info into breadcrumbs.txt, minimal overhead - will be ignored for non-amd gpus
+        inline static bool m_breadcrumbs_enabled             = false;  // tracks gpu crashes and writes info into breadcrumbs.txt, minimal overhead - will be ignored for non-amd gpus
         inline static bool m_logging_to_file_enabled         = false; // logs to file, high cpu cost due to disk i/o
         inline static bool m_renderdoc_enabled               = false; // integrates renderdoc, high cpu overhead from api wrapping
-        inline static bool m_gpu_marking_enabled             = true;  // gpu markers for debugging, no performance impact
-        inline static bool m_gpu_timing_enabled              = true;  // measures gpu timings, negligible cost
-        inline static bool m_shader_optimization_enabled     = true;  // enables shader optimizations, high cost when off
+        inline static bool m_gpu_marking_enabled             = true;  // negligible cost
+        inline static bool m_gpu_timing_enabled              = true;  // negligible cost
+        inline static bool m_shader_optimization_enabled     = true;  // high cost when off
     };
 }
