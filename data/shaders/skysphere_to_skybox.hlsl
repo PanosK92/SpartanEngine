632a53a79cbfd7366f3cd6246d5b4b1332ee6c1f
Panos Karabelas <panosconroe@hotmail.com>
2024-10-31 12:21:55 +0000

    [skysphere] it's now properly converted into a cubemap (this means that sssr and gi have a proper fallback)

--- data/shaders/skysphere_to_skybox.hlsl
+++ data/shaders/skysphere_to_skybox.hlsl
@@ -28,36 +28,30 @@ void main_cs(uint3 thread_id : SV_DispatchThreadID)
 {
     float2 resolution_out;
     tex.GetDimensions(resolution_out.x, resolution_out.y);
-
-    // Convert thread_id.xy to UV coordinates
     float2 uv = (float2(thread_id.xy) + 0.5f) / resolution_out;
-
-    // Convert UVs from cubemap to direction
+    
+    // compute direction
     float3 direction;
-    float3 abs_dir;
-    uint face_index = thread_id.z;  // cubemap face index
-
+    uint face_index = thread_id.z;
     switch (face_index)
     {
-        case 0: direction = float3(1.0f, uv.y * 2.0f - 1.0f, uv.x * 2.0f - 1.0f); break;  // +X
-        case 1: direction = float3(-1.0f, uv.y * 2.0f - 1.0f, -(uv.x * 2.0f - 1.0f)); break;  // -X
+        case 0: direction = float3(1.0f, -(uv.y * 2.0f - 1.0f), -(uv.x * 2.0f - 1.0f)); break;  // +X
+        case 1: direction = float3(-1.0f, -(uv.y * 2.0f - 1.0f), uv.x * 2.0f - 1.0f); break;  // -X
         case 2: direction = float3(uv.x * 2.0f - 1.0f, 1.0f, uv.y * 2.0f - 1.0f); break;  // +Y
         case 3: direction = float3(uv.x * 2.0f - 1.0f, -1.0f, -(uv.y * 2.0f - 1.0f)); break;  // -Y
-        case 4: direction = float3(uv.x * 2.0f - 1.0f, uv.y * 2.0f - 1.0f, 1.0f); break;  // +Z
-        case 5: direction = float3(-(uv.x * 2.0f - 1.0f), uv.y * 2.0f - 1.0f, -1.0f); break;  // -Z
+        case 4: direction = float3(uv.x * 2.0f - 1.0f, -(uv.y * 2.0f - 1.0f), 1.0f); break;  // +Z
+        case 5: direction = float3(-(uv.x * 2.0f - 1.0f), -(uv.y * 2.0f - 1.0f), -1.0f); break;  // -Z
     }
-
     direction = normalize(direction);
 
-    // convert the cubemap direction back to spherical coordinates
-    float phi   = atan2(direction.z, direction.x);
-    float theta = acos(direction.y);
-
-    // map spherical coordinates back to UV space of the input texture
+    // compute sphereical uv
     float2 spherical_uv;
-    spherical_uv.x = (phi / (2.0f * PI)) + 0.5f;
+    float phi      = atan2(direction.z, direction.x);
+    float theta    = acos(direction.y);
+    spherical_uv.x = frac((phi / (2.0f * PI)) + 0.5f);
     spherical_uv.y = 1.0f - (theta / PI);
-
-
-    tex_uav_sss[uint3(thread_id.xy, face_index)] = tex.SampleLevel(samplers[sampler_bilinear_clamp], spherical_uv, 0);
+    
+    // write
+    float4 sampled_color = tex.SampleLevel(samplers[sampler_bilinear_clamp], spherical_uv, 0);
+    tex_uav_sss[uint3(thread_id.xy, face_index)] = float4(sampled_color.rgb, 1.0f);
 }
