2701636df0b2ca36e2bc9ec0f61ef4c94ff8b3ad
Panos Karabelas <panosconroe@hotmail.com>
2024-11-11 01:38:27 +0000

    [game] enabled screen space shadows for the bistro light as there are small scale details like plates, forks etc that aren't captured that well otherwise

--- runtime/Game/Game.cpp
+++ runtime/Game/Game.cpp
@@ -808,14 +808,13 @@ namespace Spartan
             {
                 shared_ptr<Entity> light = World::CreateEntity();
                 light->SetObjectName("light_point");
-                light->SetPositionLocal(Vector3(2.2039f, 3.9965f, 3.2015f));
-                light->AddComponent<Light>()->SetFlag(LightFlags::ShadowsScreenSpace, false);
-                light->GetComponent<Light>()->SetFlag(LightFlags::ShadowsTransparent, false);
+                light->SetPositionLocal(Vector3(2.2f, 4.0f, 3.2f));
+                light->AddComponent<Light>()->SetFlag(LightFlags::ShadowsTransparent, false);
                 light->GetComponent<Light>()->SetFlag(LightFlags::Volumetric, false);
                 light->GetComponent<Light>()->SetLightType(LightType::Point);
                 light->GetComponent<Light>()->SetRange(120.0f);
                 light->GetComponent<Light>()->SetIntensity(LightIntensity::bulb_60_watt);
-                light->GetComponent<Light>()->SetTemperature(4000.0f); // a bit white, what the emissive textures seems to try to emulate
+                light->GetComponent<Light>()->SetTemperature(4000.0f); // a bit white, what the emissive textures seem to try to emulate
 
                 shared_ptr<Entity> entity = mesh->GetRootEntity().lock();
                 entity->SetObjectName("bistro_interior");
