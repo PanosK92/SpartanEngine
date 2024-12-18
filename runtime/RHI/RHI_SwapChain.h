f0b733f08a0e80f89a9ec8d67488f094de8aa089
Panos Karabelas <panosconroe@hotmail.com>
2024-12-18 14:40:02 +0000

    [vulkan] fixed a crash that could occur when resizing the main window

--- runtime/RHI/RHI_SwapChain.h
+++ runtime/RHI/RHI_SwapChain.h
@@ -94,8 +94,8 @@ namespace Spartan
         uint32_t m_image_index                                   = std::numeric_limits<uint32_t>::max();
         void* m_sdl_window                                       = nullptr;
         std::array<RHI_Image_Layout, max_buffer_count> m_layouts = { RHI_Image_Layout::Max };
-        std::array<std::shared_ptr<RHI_SyncPrimitive>, max_buffer_count> m_image_acquired_semaphore;
-        std::array<std::shared_ptr<RHI_SyncPrimitive>, max_buffer_count> m_image_acquired_fence;
+        std::array<std::unique_ptr<RHI_SyncPrimitive>, max_buffer_count> m_image_acquired_semaphore;
+        std::array<std::unique_ptr<RHI_SyncPrimitive>, max_buffer_count> m_image_acquired_fence;
         std::vector<RHI_SyncPrimitive*> m_wait_semaphores;
 
         // rhi
