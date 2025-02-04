e08cb81721a8a852429fc97aafd51dc414cfdb54
Panos Karabelas <panosconroe@hotmail.com>
2025-02-04 15:27:30 +0000

    [renderer] introduced low quality mode (true for relic gpus), for now this sets GI to off

--- runtime/Rendering/Renderer.cpp
+++ runtime/Rendering/Renderer.cpp
@@ -152,6 +152,8 @@ namespace spartan
 
         // options
         {
+            bool low_quality = RHI_Device::GetPrimaryPhysicalDevice()->IsBelowMinimumRequirments();
+
             m_options.clear();
             SetOption(Renderer_Option::WhitePoint,                  350.0f);
             SetOption(Renderer_Option::Tonemapping,                 static_cast<float>(Renderer_Tonemapping::Max));
@@ -161,7 +163,7 @@ namespace spartan
             SetOption(Renderer_Option::ScreenSpaceAmbientOcclusion, 1.0f);
             SetOption(Renderer_Option::ScreenSpaceShadows,          static_cast<float>(Renderer_ScreenspaceShadow::Bend));
             SetOption(Renderer_Option::ScreenSpaceReflections,      RHI_Device::GetPrimaryPhysicalDevice()->IsAmd() ? 1.0f : 0.0f); // temp workaround until I fix ssr for nvidia
-            SetOption(Renderer_Option::GlobalIllumination,          0.5f);                                                 // 0.5 is the percentage of the internal resolution (options are 25%, 50%, 75% and 100%)
+            SetOption(Renderer_Option::GlobalIllumination,          low_quality ? 0.0f : 0.5f);                                     // 0.5 is the percentage of the internal resolution (options are 25%, 50%, 75% and 100%)
             SetOption(Renderer_Option::Anisotropy,                  16.0f);
             SetOption(Renderer_Option::ShadowResolution,            4096.0f);
             SetOption(Renderer_Option::Sharpness,                   0.0f);                                                 // becomes the upsampler's sharpness as well
