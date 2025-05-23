c8f5410260831105830c5616d8da286410c5617f
Panos Karabelas <panosconroe@hotmail.com>
2025-05-23 16:08:50 +0100

    [rhi] made the texture read write barrier less broad since it's only used for compute and pixel shaders

--- runtime/RHI/Vulkan/Vulkan_CommandList.cpp
+++ runtime/RHI/Vulkan/Vulkan_CommandList.cpp
@@ -1782,10 +1782,10 @@ namespace spartan
 
         VkImageMemoryBarrier2 barrier           = {};
         barrier.sType                           = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2;
-        barrier.srcStageMask                    = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;                       // wait for all previous stages
-        barrier.srcAccessMask                   = VK_ACCESS_2_MEMORY_READ_BIT | VK_ACCESS_2_MEMORY_WRITE_BIT; // wait for all previous reads and writes
-        barrier.dstStageMask                    = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;                       // allow all future stages
-        barrier.dstAccessMask                   = VK_ACCESS_2_MEMORY_READ_BIT | VK_ACCESS_2_MEMORY_WRITE_BIT; // allow all future reads and writes
+        barrier.srcStageMask                    = VK_PIPELINE_STAGE_2_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT;
+        barrier.srcAccessMask                   = VK_ACCESS_2_SHADER_READ_BIT | VK_ACCESS_2_SHADER_WRITE_BIT;
+        barrier.dstStageMask                    = VK_PIPELINE_STAGE_2_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT;
+        barrier.dstAccessMask                   = VK_ACCESS_2_SHADER_READ_BIT | VK_ACCESS_2_SHADER_WRITE_BIT;
         barrier.srcQueueFamilyIndex             = VK_QUEUE_FAMILY_IGNORED;
         barrier.dstQueueFamilyIndex             = VK_QUEUE_FAMILY_IGNORED;
         barrier.image                           = static_cast<VkImage>(texture->GetRhiResource());
