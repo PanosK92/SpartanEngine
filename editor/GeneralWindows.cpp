804f09bfa0c3ddcd43c97f5d776b18c545eac2d9
Panos Karabelas <panosconroe@hotmail.com>
2025-06-03 00:03:00 +0100

    [thirdparty] updated assimp to latest

--- editor/GeneralWindows.cpp
+++ editor/GeneralWindows.cpp
@@ -103,6 +103,8 @@ namespace
         static const char* license_text =
             "MIT License"
             "\n\n"
+            "Copyright(c) 2015-2025 Panos Karabelas"
+             "\n\n"
             "Permission is hereby granted, free of charge, to any person obtaining a copy"
             "of this software and associated documentation files (the \"Software\"), to deal"
             "in the Software without restriction, including without limitation the rights"
---
+++ third_party/assimp/assimp/revision.h
@@ -0,0 +1,29 @@
+#ifndef ASSIMP_REVISION_H_INC
+#define ASSIMP_REVISION_H_INC
+
+#define GitVersion 0x0
+#define GitBranch ""
+
+#define VER_MAJOR 6
+#define VER_MINOR 0
+#define VER_PATCH 1
+#define VER_BUILD 0
+
+#define STR_HELP(x) #x
+#define STR(x) STR_HELP(x)
+
+#define VER_FILEVERSION             VER_MAJOR,VER_MINOR,VER_PATCH,VER_BUILD
+#if (GitVersion == 0)
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
+#else
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 0)"
+#endif
+#define VER_COPYRIGHT_STR           "\xA9 2006-2023"
+
+#ifdef  NDEBUG
+#define VER_ORIGINAL_FILENAME_STR   "assimp.dll"
+#else
+#define VER_ORIGINAL_FILENAME_STR   "assimpd.dll"
+#endif //  NDEBUG
+
+#endif // ASSIMP_REVISION_H_INC
