efda1e0fc4b83ffc040e1076d1da10bec44827c8
Panos Karabelas <panosconroe@hotmail.com>
2025-07-11 12:51:56 +0100

    [d3d12] build fix

--- runtime/RHI/D3D12/D3D12_Shader.cpp
+++ runtime/RHI/D3D12/D3D12_Shader.cpp
@@ -58,7 +58,7 @@ namespace spartan
         }
 
         // compile
-        if (IDxcResult* dxc_result = DirecXShaderCompiler::Compile(m_preprocessed_source, arguments))
+        if (IDxcResult* dxc_result = DirectXShaderCompiler::Compile(m_preprocessed_source, arguments))
         {
             // Get compiled shader buffer
             IDxcBlob* shader_buffer = nullptr;
