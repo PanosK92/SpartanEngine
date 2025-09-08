293f9e9e4dee420d5228bf436a798b1c800a854c
George Bolba <giksterbolba@gmail.com>
2025-09-08 21:39:40 +0200

    [shaders] updated color, closes #14

--- data/shaders/ocean/initial_spectrum.hlsl
+++ data/shaders/ocean/initial_spectrum.hlsl
@@ -3,5 +3,5 @@ RWTexture2D<float4> initial_spectrum : register(u9);
 [numthreads(8, 8, 1)]
 void main_cs(uint3 thread_id : SV_DispatchThreadID)
 {
-    initial_spectrum[thread_id.xy] = float4(1.0f, 0.0f, 1.0f, 1.0f);
+    initial_spectrum[thread_id.xy] = float4(1.0f, 1.0f, 0.0f, 1.0f);
 }
