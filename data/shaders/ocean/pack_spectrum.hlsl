c451e865f2d02d975748e135a4053b2df74c5482
George Bolba <giksterbolba@gmail.com>
2025-09-17 10:40:13 +0200

    [license] added license and code inspiration credit

--- data/shaders/ocean/pack_spectrum.hlsl
+++ data/shaders/ocean/pack_spectrum.hlsl
@@ -1,3 +1,27 @@
+/*
+Copyright(c) 2025 George Bolba
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Inspired by Acerola's Implementation:
+// https://github.com/GarrettGunnell/Water/blob/main/Assets/Shaders/FFTWater.compute
+
 #include "../common.hlsl"
 
 static const uint SPECTRUM_TEX_SIZE = 512;
@@ -7,8 +31,6 @@ RWTexture2D<float4> initial_spectrum : register(u9);
 [numthreads(8, 8, 1)]
 void main_cs(uint3 thread_id : SV_DispatchThreadID)
 {
-    // Heavily Inspired by Acerola's Implementation:
-    // https://github.com/GarrettGunnell/Water/blob/main/Assets/Shaders/FFTWater.compute
     float2 h0 = initial_spectrum[thread_id.xy].rg;
     float2 h0conj = initial_spectrum[uint2((SPECTRUM_TEX_SIZE - thread_id.x) % SPECTRUM_TEX_SIZE, (SPECTRUM_TEX_SIZE - thread_id.y) % SPECTRUM_TEX_SIZE)].rg;
 
