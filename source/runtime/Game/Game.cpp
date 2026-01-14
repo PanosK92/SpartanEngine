16b8411be10abb008aa1492f376a2008d161b1c9
Panos Karabelas <panosconroe@hotmail.com>
2026-01-14 18:46:39 +0000

    [misc] adjusted showroom lighting

--- source/runtime/Game/Game.cpp
+++ source/runtime/Game/Game.cpp
@@ -1741,7 +1741,7 @@ namespace spartan
                                     light->SetLightType(LightType::Point);
                                     light->SetColor(color);
                                     light->SetRange(40.0f);
-                                    light->SetIntensity(2000.0f);
+                                    light->SetIntensity(6000.0f);
                                     light->SetFlag(LightFlags::Shadows,            true);
                                     light->SetFlag(LightFlags::ShadowsScreenSpace, false);
                                     light->SetFlag(LightFlags::Volumetric,         false);
