cb31a590c0a8c0a7fcdb5b45a8e67158bc17f10a
Panos Karabelas <panosconroe@hotmail.com>
2026-02-05 05:32:21 +0000

    [third_party] downgraded lua to 5.4.8 which is compatible with sol

--- build_scripts/generate_project_files.py
+++ build_scripts/generate_project_files.py
@@ -86,8 +86,8 @@ def main():
     file_utilities.copy(Path("build_scripts") / "7z.dll", "binaries")
 
     print("\n2. Download and extract libraries...")
-    library_url           = 'https://www.dropbox.com/scl/fi/m9rxnzmfcilvm5fls1f9x/libraries.7z?rlkey=hj2vm08uapfnosjiwxo7acxln&st=f5ijgn9x&dl=1'
-    library_expected_hash = '7ae5b917e37acbcdd3f45d1de6118bfcf8733f3e50872eb012632cd50a8c4b83'
+    library_url           = 'https://www.dropbox.com/scl/fi/p4c3nxx89xjdd5letdblw/libraries.7z?rlkey=i71b8403gjvv8t0l5nox1knsg&st=br54pnqf&dl=1'
+    library_expected_hash = '01e7978852c3d2f6925423e540d97ba3ef3734f7094d12b78aacbc3852b7d6dd'
     library_destination   = Path("third_party") / "libraries" / "libraries.7z"
     file_utilities.download_file(library_url, str(library_destination), library_expected_hash)
     file_utilities.extract_archive(str(library_destination), str(Path("third_party") / "libraries"))
